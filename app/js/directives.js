7844c0210bc2b4caa60d559ac848009c2450f08a
Anders Riutta <anders.riutta@gmail.com>
2013-06-12 19:10:28 -0700

    svgPan not working

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,44 +3,51 @@
 /* Directives */
 
 angular.module('myApp.directives', [])
-.directive('btnEditable', ['pathwayService', function(pathwayService) {
+.directive('btnEditable', ['drawingParameters', function(drawingParameters) {
 	return function($scope, elm, attrs, $location) {
 		$scope.$watch('editable', function(editable) {
 			if (editable) {
 				if ($scope.editable == true) {
+					drawingParameters.editable = true;
 					parent.document.getElementById('pathwayFrame').src = "../../app/#/wpEditor?wgTitle=WP299&wgCurRevisionId=61677";
-					$('#viewport').attr("transform", "scale(1)")
 				}
 				else {
+					drawingParameters.editable = false;
 					parent.document.getElementById('pathwayFrame').src = "../../app/#/wpViewer?wgTitle=WP299&wgCurRevisionId=61677";
 				}
 			}
 		});
 	}
 }])
-.directive('btnViewSize', ['pathwayService', function(pathwayService) {
+.directive('btnViewSize', [function() {
 	return function($scope, elm, attrs) {
-		$scope.$watch('viewSize', function(viewSize) {
+		$scope.$watch('drawingParameters.viewSize', function(viewSize) {
+			console.log("$scope in btnViewSize");
+			console.log($scope);
+			//alert(viewSize);
+			//alert("enableZoom in btnViewSize: " + $scope.drawingParameters.enableZoom);
 			if (viewSize) {
-				if ($scope.viewSize == 'fullscreen') {
+				if (viewSize == 'fullscreen') {
+					alert('btnViewSize: fullscreen');
+					$scope.drawingParameters.enableZoom = 1;
 					fullScreenApi.requestFullScreen(parent.document.getElementById('pathwayFrame'));
-					$scope.$parent.enableZoom = 1;
 				}
 				else {
-					if ($scope.viewSize == 'large') {
+					if (viewSize == 'large') {
 						fullScreenApi.cancelFullScreen();
-						$scope.$parent.enableZoom = 1;
+						$scope.drawingParameters.enableZoom = 1;
 						alert('Sorry, Large View not yet functional.');
 					}
 					else {
-						if ($scope.viewSize == 'small') {
+						if (viewSize == 'small') {
 							fullScreenApi.cancelFullScreen();
-							$scope.$parent.enableZoom = 0;
+							alert('btnViewSize: small');
+							$scope.drawingParameters.enableZoom = 0;
 						}
 					}
 				}
 			}
-		});
+		}, true);
 	}
 }])
 // this directive is not currently in use. It would be nice to move the SVG script in view.html to this location.
@@ -56,7 +63,7 @@ angular.module('myApp.directives', [])
 		}
 	};
 })
-.directive('drawingBoard', ['pathwayService', function(pathwayService) {
+.directive('drawingBoard', ['drawingParameters', function(drawingParameters) {
 	return function($scope, elm, attrs) {
 		// I don't remember what this is for - maybe checking whether I'm inside an iframe?
 		function objToString (obj) {
@@ -69,61 +76,43 @@ angular.module('myApp.directives', [])
 			return str;
 		}
 
-		$scope.$parent.enablePan = 1;
-		$scope.$parent.enableZoom = 0;
-		$scope.$parent.enableDrag = 0;
-		$scope.$parent.zoomScale = .2;
-		
 		// Define svg
 		//$scope.$watch(function() { return angular.toJson(['pathways.Pathway["@Name"]', 'editable']) }, function(pathway) {
 		$scope.$watch('pathways.Pathway["@Name"]', function() {
-		console.log($scope);
-		var cscope = self.cscope = $scope;
+			console.log("$scope inside drawingBoard");
+			console.log($scope);
 			if ($scope.pathways)
 				{
-					console.log("inside");
+					console.log("$scope inside drawingBoard if statement");
 					console.log($scope);
-					console.log($scope.editable);
-					console.log($scope.$parent.editable);
-					console.log($scope.pathways);
-					console.log($scope.pathways.Pathway);
-					console.log($scope.pathways.Pathway.Graphics["@BoardWidth"]);
-					//console.log($scope.pathways.Pathway["@Name"]);
-					//elm.attr("style", "background-color: beige; display: block; position:absolute; height:auto; bottom:0; top:0; left:0; right:0; margin-top:0; margin-bottom:0; margin-right:0; margin-left:0;");
-				//	elm.attr("preserveAspectRatio", "xMidYMid");
-				       // removed position:absolute and display:block in order to not break the pan ability
+					console.log("drawingParameters.editable");
+					console.log(drawingParameters.editable);
 					elm.attr("style", "background-color: #fff0ff; height:auto; bottom:0; top:0; left:0; right:0; margin-top:0; margin-bottom:0; margin-right:0; margin-left:0;");
-				       // it appears the g viewport container is messing up the viewbox somehow
-				//	elm.attr("viewBox", "0 0 " + $scope.pathways.Pathway.Graphics["@BoardWidth"] + " " + $scope.pathways.Pathway.Graphics["@BoardHeight"]);
 			       		var scaleViewAll = Math.min(elm[0].clientWidth/$scope.pathways.Pathway.Graphics["@BoardWidth"], elm[0].clientHeight/$scope.pathways.Pathway.Graphics["@BoardHeight"]);
-
-					if ($scope.$parent.editable == true) {
-						//$('#viewport').attr("transform", "scale(1)")
-						alert("true: " + $scope.editable);
-						//fullScreenApi.requestFullScreen(parent.parent.document.getElementById('pathwayFrame'))
+					if (drawingParameters.editable == true) {
+						$('#viewport').attr("transform", "scale(1)")
 					}
 					else {
-						//$('#viewport').attr("transform", "scale(" + scaleViewAll + ")")
-						//svg.attr("viewBox", "0 0 " + $scope.pathway.width + " " + $scope.pathway.height)
-						alert("else: " + $scope.editable);
+						$('#viewport').attr("transform", "scale(" + scaleViewAll + ")")
 					};
 				}
 		}, true)
+		$scope.$watch('drawingParameters.enableZoom', function(enableZoom) {
+			console.log("enableZoom");
+			console.log(enableZoom);
+			//alert("$scope.drawingParameters.enableZoom in drawingBoard: " + $scope.drawingParameters.enableZoom);
+			//alert("enableZoom in drawingBoard: " + enableZoom);
+			elm.svgPan('viewport', $scope.drawingParameters.enablePan, enableZoom, $scope.drawingParameters.enableDrag, $scope.drawingParameters.zoomScale);
+		});
 	}
 }
 ])
-.directive('node', ['pathwayService', function(pathwayService) {
+.directive('node', [function() {
 	return function($scope, elm, attrs) {
-		//console.log("you");
-		//console.log("elm");
-		//console.log(elm[0]);
-		//console.log($scope);
-		//elm[0].id = $scope.DataNode["@GraphId"];
 		elm[0].setAttribute("transform", "translate(" + $scope.DataNode.Graphics.x + "," + $scope.DataNode.Graphics.y + ")");
-
 	}
 }])
-.directive('nodeBoundingBox', ['pathwayService', function(pathwayService) {
+.directive('nodeBoundingBox', [function() {
 	return function($scope, elm, attrs) {
 		//console.log("nodeBoundingBox elm");
 		//console.log(elm[0]);
@@ -139,7 +128,7 @@ angular.module('myApp.directives', [])
 		elm[0].setAttribute("fill", $scope.DataNode.Graphics["@FillColor"]);
 	}
 }])
-.directive('nodeLabel', ['pathwayService', function(pathwayService) {
+.directive('nodeLabel', [function() {
 	return function($scope, elm, attrs) {
 		$scope.$watch('pathways.Pathway.DataNode["@TextLabel"]', function() {
 			if ($scope.pathways)
--- app/js/services.js
+++ app/js/services.js
@@ -7,18 +7,18 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', ['ngResource'])
 	.value('pathwayService', 'pathwaytext')
+	.value('drawingParameters', {editable:false, enablePan:1, enableZoom:0, enableDrag:0, zoomScale:0.2 })
 	.factory('Pathway', function($location, $http){
-
-   return {
-       getSource: function(callback) {
-          //var url = $location.search().dataSrc;
-          var url = "../samples/gpml/" + $location.search().wgTitle + "_" + $location.search().wgCurRevisionId  + ".gpml";
-	  console.log(url)
-          $http.get(url).success(function(data) {
-             callback(data);
-          })
-       }
-   }
+	   return {
+	       getSource: function(callback) {
+		  //var url = $location.search().dataSrc;
+		  var url = "../samples/gpml/" + $location.search().wgTitle + "_" + $location.search().wgCurRevisionId  + ".gpml";
+		  console.log(url)
+		  $http.get(url).success(function(data) {
+		     callback(data);
+		  })
+	       }
+	   }
 
 	/* I would like to use $resource, but this isn't working now.
 	return $scope.pathway
