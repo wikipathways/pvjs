ebf4e934721aafb9762cca189b57336cc5ae00bb
Anders Riutta <anders.riutta@gmail.com>
2013-06-19 15:14:19 -0700

    view looking ok. programming not done.

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,11 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ui.bootstrap']).
   config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/wpViewer', {templateUrl: 'partials/wpViewer.html', controller: 'PathwayCtrl'});
-	$routeProvider.when('/wpEditor', {templateUrl: 'partials/wpEditor.html', controller: 'PathwayCtrl'});
+	$routeProvider.when('/wpViewer', {templateUrl: 'index.html', controller: 'PathwayCtrl'});
+	$routeProvider.when('/wpEditor', {templateUrl: 'index.html', controller: 'PathwayCtrl'});
 	$routeProvider.when('/widgetViewer', {templateUrl: 'partials/widgetViewer.html', controller: 'PathwayCtrl'});
-	$routeProvider.otherwise({redirectTo: '/'});
+	$routeProvider.when('/', {templateUrl: 'index.html', controller: 'PathwayCtrl'});
+	$routeProvider.otherwise({redirectTo: 'error.html'});
   }]);
