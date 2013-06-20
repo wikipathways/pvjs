0cd45ce3b6300b95caab55a2e8c89d9c77416996
Anders Riutta <anders.riutta@gmail.com>
2013-06-20 16:15:17 -0700

    moved pathway data retrieval code to service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,43 +7,9 @@ angular.module('pathvisio.controllers', [])
 	return $scope;
 
 }])
-.controller('PathwayCtrl', ['$scope', 'Pathway', function($scope, Pathway) {
-	// I would like to put this code in the service, but doing so gave me errors.
-	// how should I define $scope.pathways - here or inside the function?
-	//$scope.pathways = Pathway.getSource(function(data) {
-	Pathway.getSource(function(data) {
-		var sMyString = data;
-		var oParser = new DOMParser();
-		var oDOM = oParser.parseFromString(sMyString, "text/xml");
+.controller('PathwayCtrl', ['$scope', '$location', '$http', 'Pathway', function($scope, $location, $http, Pathway) {
 
-		var json = xml2json(oDOM, "");
-
-		var pathway = jQuery.parseJSON(json);
-
-		pathway.Pathway.DataNode.forEach(function(element, index, array) {
-			element.Graphics["x"] = parseFloat(element.Graphics["@CenterX"]) - parseFloat(element.Graphics["@Width"])/2;
-			element.Graphics["y"] = parseFloat(element.Graphics["@CenterY"]) - parseFloat(element.Graphics["@Height"])/2;
-			delete element.Graphics["@CenterX"];
-			delete element.Graphics["@CenterY"];
-			if (element.Graphics["@FillColor"]) {
-				element.Graphics["@FillColor"] = "#" + element.Graphics["@FillColor"]
-			}
-			else {
-				element.Graphics["@FillColor"] = "white";
-			};	
-			if (element.Graphics["@Color"]) {
-				element.Graphics["@Color"] = "#" + element.Graphics["@Color"]
-			}
-			else {
-				element.Graphics["@Color"] = "black";
-			};	
-		});
-
-		$scope.pathways = pathway;
-		// This doesn't seem needed
-		// return pathway;
-
-	});
+	Pathway.getData($scope);
 
 	if (!($scope.drawingParameters)) {
 		$scope.drawingParameters = {};
--- app/js/services.js
+++ app/js/services.js
@@ -2,32 +2,75 @@
 
 /* Services */
 
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
 angular.module('pathvisio.services', ['ngResource'])
-	.value('pathwayService', 'pathwaytext')
-	.factory('Pathway', function($location, $http){
-	   return {
-	       getSource: function(callback) {
-		  //var url = $location.search().dataSrc;
-		  if (!($location.search().wgTitle)) {
-			  var url = "../samples/gpml/WP673_63184.gpml";
-		  }
-		  else {
-			  var url = "../samples/gpml/" + $location.search().wgTitle + "_" + $location.search().wgCurRevisionId  + ".gpml";
-		  };
-		  return $http.get(url).success(function(data) {
-		     callback(data);
-		  })
-	       }
-	   }
+.factory('Pathway', function($location, $http){
+	return {
+		getData: function($scope) {
+			if (!($location.search().wgTitle)) {
+				var url = "../samples/gpml/WP673_63184.gpml";
+			}
+			else {
+				var url = "../samples/gpml/" + $location.search().wgTitle + "_" + $location.search().wgCurRevisionId  + ".gpml";
+			};
+			function getDataFile(url, callback) {
+				$http.get(url).success(function(data) {
+					return callback(data);
+				})
+			}
+			getDataFile(url, function(data) {
+				function Right(str, n){
+					if (n <= 0)
+						return "";
+					else if (n > String(str).length)
+						return str;
+					else {
+						var iLen = String(str).length;
+						return String(str).substring(iLen, iLen - n);
+					}
+				}
+				if (Right(url,4) == "gpml") {
+					var sMyString = data;
+					var oParser = new DOMParser();
+					var oDOM = oParser.parseFromString(sMyString, "text/xml");
+
+					var json = xml2json(oDOM, "");
+
+					var pathway = jQuery.parseJSON(json);
+
+					pathway.Pathway.DataNode.forEach(function(element, index, array) {
+						element.Graphics["x"] = parseFloat(element.Graphics["@CenterX"]) - parseFloat(element.Graphics["@Width"])/2;
+						element.Graphics["y"] = parseFloat(element.Graphics["@CenterY"]) - parseFloat(element.Graphics["@Height"])/2;
+						delete element.Graphics["@CenterX"];
+						delete element.Graphics["@CenterY"];
+						if (element.Graphics["@FillColor"]) {
+							element.Graphics["@FillColor"] = "#" + element.Graphics["@FillColor"]
+						}
+						else {
+							element.Graphics["@FillColor"] = "white";
+						};	
+						if (element.Graphics["@Color"]) {
+							element.Graphics["@Color"] = "#" + element.Graphics["@Color"]
+						}
+						else {
+							element.Graphics["@Color"] = "black";
+						};	
+					});
+
+					$scope.pathways = pathway;
+				}
+				else {
+					console.log("file type error")
+				}
+			});
+
+		}
+	}
 
 	/* I would like to use $resource, but this isn't working now.
-	return $scope.pathway
-		return $resource('../samples/gpml/DatanodeShapes.gpml', {}, {
-			query: {method:'GET', headers:{'Content-Type':'text/xml'}}
-		});
-	       */
+	   return $scope.pathway
+	   return $resource('../samples/gpml/DatanodeShapes.gpml', {}, {
+query: {method:'GET', headers:{'Content-Type':'text/xml'}}
+});
+*/
 });
 
