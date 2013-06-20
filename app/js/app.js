bd312d4ab86e0423c23bb112883a558e474d2610
Anders Riutta <anders.riutta@gmail.com>
2013-06-19 17:50:57 -0700

    updated app name from myApp to pathvisio

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ui.bootstrap']).
+angular.module('pathvisio', ['pathvisio.filters', 'pathvisio.services', 'pathvisio.directives', 'pathvisio.controllers', 'ui.bootstrap']).
   config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/wpViewer', {templateUrl: 'index.html', controller: 'PathwayCtrl'});
 	$routeProvider.when('/wpEditor', {templateUrl: 'index.html', controller: 'PathwayCtrl'});
