eb48cf8068e95b98e39d1a33fe41e7ab1ca4f9a3
Anders Riutta <anders.riutta@gmail.com>
2013-06-25 12:01:30 -0700

    updated e2e tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,22 +2,66 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('pathvisio', function() {
+describe('wikipathways version', function() {
 
   beforeEach(function() {
     browser().navigateTo('/samples/wikipathways-version/index.html');
   });
 
-    it('should render iframe#pathwayFrame when user navigates to samples/wikipathways-version/', function() {
-	    /*
-	    console.log(element('body'));
-	    console.log(browser().window().href());
-	    console.log(browser().window().path());
-	    console.log(browser().location().url());
-	    console.log(browser().location().path());
-	   */
-      //expect(element('body').count()).toEqual(1);
-      expect(element('#globalWrapper').count()).toEqual(1);
-      //expect(element('iframe').count()).toEqual(1);
+    it('should have 1 body when user navigates to samples/wikipathways-version/', function() {
+      expect(element('body').count()).toEqual(1);
     });
+
+    it('should have 1 iframe#pathwayFrame when user navigates to samples/wikipathways-version/', function() {
+      expect(element('iframe#pathwayFrame').count()).toEqual(1);
+    });
+
+});
+
+describe('viewer', function() {
+
+  beforeEach(function() {
+    browser().navigateTo('/app/index.html#/?wgTitle=WP673&wgCurRevisionId=63184');
+  });
+
+    it('should have 1 svg#drawingBoard', function() {
+      expect(element('svg#drawingBoard').count()).toEqual(1);
+    });
+
+    it('should have 2 Metabolites', function() {
+      expect(element('g.Metabolites').count()).toEqual(2);
+    });
+
+    it('should have 5 Pathways', function() {
+      expect(element('g.Pathway').count()).toEqual(5);
+    });
+
+    it('should have 68 GeneProducts', function() {
+      expect(element('g.GeneProduct').count()).toEqual(68);
+    });
+
+    it('should have an svg#drawingBoard with height equal to the height of the body', function() {
+	var bodyHeight = element('body').height();
+	var drawingBoardHeight = element('#drawingBoard').height();
+      expect(bodyHeight).toEqual(drawingBoardHeight);
+    });
+
+    /*
+    it('should have an svg#drawingBoard with height 657', function() {
+      expect( element('body').height() ).toEqual(1);
+    });
+
+    it('should have an svg#drawingBoard with height equal to window height', function() {
+	var body = document.body,
+	    html = document.documentElement;
+
+	var height = Math.max( body.scrollHeight, body.offsetHeight, 
+			       html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+    var ratioBodyToSvg = height / document.getElementById('drawingBoard').getBBox().height;
+    console.log(ratioBodyToSvg);
+
+      expect(ratioBodyToSvg).toEqual(1);
+    });
+*/
 });
