8b907487fcf8b26dfe92c85e264f842272c8733f
Anders Riutta <anders.riutta@gmail.com>
2013-08-06 11:20:29 -0700

    updated edges

--- app/js/gpml2json.js
+++ app/js/gpml2json.js
@@ -623,6 +623,7 @@ function convertGpml2Json(xmlDoc){
               delete element.relx;
               delete element.x;
 
+
               element.relY = anchorPositionMappings[element.rely.toString()];
               delete element.rely;
               delete element.y;
