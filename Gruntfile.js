3a6135efb6cd202d3c2862e5f6fc6423d52c52d7
Anders Riutta <git@andersriutta.com>
2013-08-29 13:27:52 -0700

    started working on deployment setup with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,22 +2,46 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: [
+          'src/js/jxon.js',
+          'src/js/rgbcolor.js',
+          'src/js/pathvisio/helpers.js',
+          'src/js/pathvisio/pathvisio.js',
+          'src/js/pathvisio/pathway/pathway.js',
+          'src/js/pathvisio/pathway/group.js',
+          'src/js/pathvisio/pathway/info-box.js',
+          'src/js/pathvisio/pathway/labelable-element.js',
+          'src/js/pathvisio/pathway/edge/edge.js',
+          'src/js/pathvisio/pathway/edge/marker.js',
+          'src/js/pathvisio/pathway/edge/path-data.js',
+          'src/js/pathvisio/pathway/edge/point.js',
+        ],
+        dest: 'build/js/pathvisio-js.js'
+      },
+    },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        mangle: false
       },
-      build: {
-        src: 'src/js/<%= pkg.name %>.js',
-        dest: 'build/js/<%= pkg.name %>.min.js'
+      my_target: {
+        files: {
+          'build/js/pathvisio-js.min.js': ['build/js/pathvisio-js.js']
+        }
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
+  grunt.registerTask('default', ['concat']);
   grunt.registerTask('default', ['uglify']);
 
 };
