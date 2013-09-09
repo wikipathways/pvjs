a14834c2eb5a28f5ff232aa35bf6665cfe3f7e3b
Anders Riutta <git@andersriutta.com>
2013-09-08 17:57:41 -0700

    everything is now working in IE8 and modern browsers. still getting an error in IE8 about callbacks not being defined

--- src/views/render-png.php
+++ src/views/render-png.php
@@ -44,17 +44,36 @@ body {
   <script src="../lib/es5-shim/es5-shim.js"></script>
   <script src="../lib/Xccessors/xccessors-standard.js"></script>
 
-<![endif]-->
-
   <script>
 
     // IE8 only allows console.log when Developer Tools is open. This will prevent errors
     // from showing up if I use console.log without DevTools being open.
+    // from http://stackoverflow.com/questions/3326650/console-is-undefined-error-for-internet-explorer
 
-if (typeof console == "undefined") {
-    this.console = {log: function() {}};
-}
+    /**
+     * Protect window.console method calls, e.g. console is not defined on IE
+     * unless dev tools are open, and IE doesn't define console.debug
+     */
+    (function() {
+      if (!window.console) {
+        window.console = {};
+      }
+      // union of Chrome, FF, IE, and Safari console methods
+      var m = [
+        "log", "info", "warn", "error", "debug", "trace", "dir", "group",
+        "groupCollapsed", "groupEnd", "time", "timeEnd", "profile", "profileEnd",
+        "dirxml", "assert", "count", "markTimeline", "timeStamp", "clear"
+      ];
+      // define undefined methods as noops to prevent errors
+      for (var i = 0; i < m.length; i++) {
+        if (!window.console[m[i]]) {
+          window.console[m[i]] = function() {};
+        }    
+      } 
+    })();
   </script>
+
+<![endif]-->
 
 </head>
 <body>
@@ -210,7 +229,7 @@ else {
     });
 
     var pathwayContainer = d3.select('#pathway-container');
-    pathwayContainer.select('svg').remove();
+    pathwayContainer.select('#pathway-image').remove();
     pathwayContainer.attr('style', 'width: 100%; height:500px');
     //pathwayContainer.attr('style', 'width:1000px; height:693px');
 
