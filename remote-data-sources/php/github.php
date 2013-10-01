1f5f691c4f93d71762594612aa5cf7f2ad72f139
Anders Riutta <git@andersriutta.com>
2013-10-01 15:27:52 -0700

    updating dev system

--- remote-data-sources/php/github.php
+++ remote-data-sources/php/github.php
@@ -1,30 +1,52 @@
 <?php
+
+$approvedRepos = array(
+  'wikipathways',
+  'AlexanderPico',
+  'andrawaag',
+  'ariutta',
+  'khanspers',
+  'mkutmon'
+);
+
 $repo = "wikipathways";
 if (isset($_GET['repo'])) {
-  if ($_GET['repo'] != "null") {
-    $repo = htmlspecialchars($_GET['repo']);
+  $repoParam = htmlspecialchars($_GET['repo']);
+  if (($repoParam != "null") && (in_array($repoParam, $approvedRepos))) {
+    $repo = $repoParam;
   }
 }
 
 $branch = "dev";
 if (isset($_GET['branch'])) {
-  if ($_GET['branch'] != "null") {
-    $branch = htmlspecialchars($_GET['branch']);
+  $branchParam = htmlspecialchars($_GET['branch']);
+  if ($branchParam != "null") {
+    $branch = $branchParam;
   }
 }
 
-// SVG pathway template
 
-if ($_GET['data']=='svg') {
-  //header("Access-Control-Allow-Origin: *");
-  $pathwayTemplateSvgUrl = "https://raw.github.com/" . $repo . "/pathvisio.js/" . $branch . "/src/views/pathway-template.svg";
-  $pathwayTemplateSvg = simplexml_load_file($pathwayTemplateSvgUrl);
+$relativeUrl = "/src/views/pathway-template.svg";
+if (isset($_GET['relativeUrl'])) {
+  $relativeUrlParam = htmlspecialchars($_GET['relativeUrl']);
+  if ($relativeUrlParam != "null") {
+    $relativeUrl = $relativeUrlParam;
+  }
+}
 
-  header("Content-Type: image/svg+xml");
-  echo $pathwayTemplateSvg->saveXML();
+$mimeType = "image/svg+xml";
+if (isset($_GET['mimeType'])) {
+  $mimeTypeParam = htmlspecialchars($_GET['mimeType']);
+  if ($mimeTypeParam != "null") {
+    $mimeType = $mimeTypeParam;
+  }
 }
-else {
-  echo "Please specify a 'data' parameter, such as '?data=svg'.";
-}
+
+//header("Access-Control-Allow-Origin: *");
+$fileUrl = "https://raw.github.com/" . $repo . "/pathvisio.js/" . $branch . $relativeUrl;
+$fileContents = file_get_contents($fileUrl);
+
+header("Content-Type: " . $mimeType);
+echo $fileContents;
 
 ?>
