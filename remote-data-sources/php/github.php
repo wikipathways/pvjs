9f17adbcc229c05a5df3e109d2d7a2d5b2abd11d
Anders Riutta <git@andersriutta.com>
2013-10-08 15:35:04 -0700

    remove references to my local dev env

--- remote-data-sources/php/github.php
+++ remote-data-sources/php/github.php
@@ -25,8 +25,7 @@ if (isset($_GET['branch'])) {
   }
 }
 
-$baseUrl = "https://raw.github.com/" . $account . "/pathvisio.js/" . $branch . '/src/';
-
+$baseUrl = "https://rawgithub.com/" . $account . "/pathvisio.js/" . $branch . '/src/';
 $relativeUrl = "views/pathway-template.svg";
 if (isset($_GET['relativeUrl'])) {
   $relativeUrlParam = htmlspecialchars($_GET['relativeUrl']);
@@ -35,13 +34,28 @@ if (isset($_GET['relativeUrl'])) {
   }
 }
 
-$mimeType = "image/svg+xml";
 if (isset($_GET['mimeType'])) {
   $mimeTypeParam = htmlspecialchars($_GET['mimeType']);
   if ($mimeTypeParam != "null") {
     $mimeType = $mimeTypeParam;
   }
 }
+elseif (substr($relativeUrl, -4) == "html" || substr($relativeUrl, -3) == "htm") {
+  $mimeType = "text/plain";
+}
+elseif (substr($relativeUrl, -3) == "css") {
+  $mimeType = "text/css";
+}
+elseif (substr($relativeUrl, -3) == "svg") {
+  $mimeType = "image/svg+xml";
+}
+elseif (substr($relativeUrl, -4) == "json") {
+  $mimeType = "text/plain";
+}
+else {
+  $mimeType = "image/svg+xml";
+}
+
 header("Content-Type: " . $mimeType);
 //header("Access-Control-Allow-Origin: *");
 
@@ -53,6 +67,9 @@ if ($mimeType == "image/svg+xml") {
   $fileContents = preg_replace("/\.\.\/\.\.\/css\/pathway-template\.css/", $cssUrl, $fileContents);
   $xml = simplexml_load_string($fileContents);
   echo $xml->saveXML();
+}
+else {
+  echo $fileContents;
 }
 
 ?>
