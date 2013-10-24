90c04ab43d433d1122f317e518121d0894f50624
Anders Riutta <git@andersriutta.com>
2013-10-24 11:25:07 -0700

    code for converting gpml to renderable json underway

---
+++ src/js/pathvisio/converter/gpml/versions.js
@@ -0,0 +1,8 @@
+// array of GPML xml namespaces in order from newest to oldest
+
+pathvisio.converter.gpml.namespaces = [
+  "http:pathvisio.org/GPML/2013a",
+  "http:genmapp.org/GPML/2010a",
+  "http:genmapp.org/GPML/2008a",
+  "http:genmapp.org/GPML/2007"
+]
