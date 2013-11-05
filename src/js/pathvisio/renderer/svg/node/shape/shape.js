930da99910377fb9f635fda90526f551d397dfbe
Anders Riutta <git@andersriutta.com>
2013-11-04 17:16:41 -0800

    working on rendering pathway using correct d3 methods

--- src/js/pathvisio/renderer/svg/node/shape/shape.js
+++ src/js/pathvisio/renderer/svg/node/shape/shape.js
@@ -1,4 +1,14 @@
 pathvisio.renderer.svg.node.shape = function(){
+  function getElementType(shapeType, scalableShapesList) {
+    if (scalableShapesList.indexOf(shapeType) > -1) {
+      elementType = 'use';
+    }
+    else {
+      elementType = 'path';
+    }
+    return elementType;
+  }
+
   function render(nodes, pathway, scalableShapesList) {
     if (!nodes || !pathway) {
       if (!nodes) {
@@ -11,8 +21,40 @@ pathvisio.renderer.svg.node.shape = function(){
     }
 
 
-    pathvisio.renderer.svg.node.shape.scalable.render(nodes, pathway, scalableShapesList);
+    //not sure whether to break this up into separate classes like immediately below
+    //pathvisio.renderer.svg.node.shape.scalable.render(nodes, pathway, scalableShapesList);
+    // this doesn't work yet
     //pathvisio.renderer.svg.node.shape.nonscalable.render(nodes, pathway, scalableShapesList);
+
+    // or do it all here with functions for specifying element type, etc. (This doesn't work yet below.)
+    if (!nodes || !pathway || !scalableShapesList) {
+      console.log(scalableShapesList);
+      if (!nodes) {
+        console.log('nodes not specified');
+      }
+      if (!pathway) {
+        console.log('pathway not specified');
+      }
+      if (!scalableShapesList) {
+        console.log('scalableShapesList not specified');
+      }
+      return console.warn('Error: Missing one or more required parameters: nodes, pathway or scalableShapesList.');
+    }
+
+    // Update… 
+    var shapes = nodes.selectAll(".shape")
+    .data(function(d) {return d;})
+    .attr('class', 'shape')
+
+    // Enter…
+    shapes.enter().append(function(d) { return getElementType(d.shapeType, scalableShapesList); })
+    .attr('class', 'shape');
+
+    // Exit…
+    shapes.exit().remove();
+
+
+
   }
 
   return {
