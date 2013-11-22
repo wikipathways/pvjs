1ab83b4af37990abd806208a1d8b3ff9c68ee4a3
Alexander Pico <apico@gladstone.ucsf.edu>
2013-11-21 18:10:59 -0800

    Update x-ref.js

--- src/js/pathvisiojs/view/annotation/x-ref.js
+++ src/js/pathvisiojs/view/annotation/x-ref.js
@@ -1,10 +1,11 @@
 pathvisiojs.view.annotation.xRef = function(){
   function render(organism, node) {
     //call once to display panel with node name
-    var annotationData = {
-          "header": node.textLabel.text
-    }
-    pathvisiojs.view.annotation.render(annotationData);
+    var dummyData = {
+          "header": node.textLabel.text,
+          "description": 'testing'
+    };
+    pathvisiojs.view.annotation.render(dummyData);
     
     //call a second time to display panel with bridgedb data
     var xRefData = pathvisiojs.data.bridgedb.getXrefAnnotationDataByDataNode(organism, node, function(annotationData) {
