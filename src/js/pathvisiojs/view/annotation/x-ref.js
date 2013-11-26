6ef2022074b1387a57677e2d2bc9ea7318f81508
Alex Pico <apico@pointer.ucsf.edu>
2013-11-26 13:24:43 -0800

    add caching mechanism for annotationData

--- src/js/pathvisiojs/view/annotation/x-ref.js
+++ src/js/pathvisiojs/view/annotation/x-ref.js
@@ -1,8 +1,25 @@
 pathvisiojs.view.annotation.xRef = function(){
+  var cachedAnnotationData = {};
+
   function render(organism, id, datasource, label, desc) {
-    var xRefData = pathvisiojs.data.bridgedb.getXrefAnnotationDataByDataNode(organism, id, datasource, label, desc, function(annotationData) {
-      pathvisiojs.view.annotation.render(annotationData);
-    });
+    var data = getCachedAnnotationData(organism, id, datasource);
+    if (data){
+      pathvisiojs.view.annotation.render(data);
+    }
+    else {
+      var xRefData = pathvisiojs.data.bridgedb.getXrefAnnotationDataByDataNode(organism, id, datasource, label, desc, function(annotationData) {
+        setCachedAnnotationData(organism, id, datasource, annotationData);
+	pathvisiojs.view.annotation.render(annotationData);
+      });
+    }
+  }
+
+  function getCachedAnnotationData(organism, id, datasource){
+    return cachedAnnotationData[organism+id+datasource];
+  }
+
+  function setCachedAnnotationData(organism, id, datasource, data){
+    cachedAnnotationData[organism+id+datasource] = data;
   }
 
   return {
