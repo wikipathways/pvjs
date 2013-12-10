573cad292ead210f9461b0c7ca69ece8783cbd64
Anders Riutta <git@andersriutta.com>
2013-12-10 11:30:01 -0800

    started working on detecting port location when entityNode is rotated

--- src/js/pathvisiojs/data/gpml/text.js
+++ src/js/pathvisiojs/data/gpml/text.js
@@ -1,34 +1,39 @@
 pathvisiojs.data.gpml.text = function() {
-  function toRenderableJson(gpmlNode, callback) {
+  function toRenderableJson(gpmlNode, textCallbackOutside) {
     try {
-      jsonText = {};
-      jsonText["tspan"] = gpmlNode.attr('TextLabel').split(/\r\n|\r|\n|&#xA;/g);
-      var textAlign = gpmlNode.select('Graphics').attr('Align') || 'center';
-      jsonText["textAlign"] = textAlign.toLowerCase();
-      var verticalAlign = gpmlNode.select('Graphics').attr('Valign') || 'top';
-      jsonText["verticalAlign"] = verticalAlign.toLowerCase();
-      var fontSize = gpmlNode.select('Graphics').attr('FontSize') || 10;
-      jsonText["fontSize"] = parseFloat(fontSize);
-      var fontStyle = gpmlNode.select('Graphics').attr('FontStyle') || 'normal';
-      jsonText["fontStyle"] = fontStyle.toLowerCase();
-      var fontWeight = gpmlNode.select('Graphics').attr('FontWeight') || 'normal';
-      jsonText["fontWeight"] = fontWeight.toLowerCase();
-      jsonText["fontFamily"] = gpmlNode.select('Graphics').attr('FontName') || 'Arial';
+      var text = gpmlNode.attr('TextLabel')
+      if (!!text) {
+        jsonText = {};
+        jsonText["tspan"] = text.split(/\r\n|\r|\n|&#xA;/g);
+        var textAlign = gpmlNode.select('Graphics').attr('Align') || 'center';
+        jsonText["textAlign"] = textAlign.toLowerCase();
+        var verticalAlign = gpmlNode.select('Graphics').attr('Valign') || 'top';
+        jsonText["verticalAlign"] = verticalAlign.toLowerCase();
+        var fontSize = gpmlNode.select('Graphics').attr('FontSize') || 10;
+        jsonText["fontSize"] = parseFloat(fontSize);
+        var fontStyle = gpmlNode.select('Graphics').attr('FontStyle') || 'normal';
+        jsonText["fontStyle"] = fontStyle.toLowerCase();
+        var fontWeight = gpmlNode.select('Graphics').attr('FontWeight') || 'normal';
+        jsonText["fontWeight"] = fontWeight.toLowerCase();
+        jsonText["fontFamily"] = gpmlNode.select('Graphics').attr('FontName') || 'Arial';
 
-      var color;
-      var colorValue = gpmlNode.select('Graphics').attr('Color');
-      if (!!colorValue) {
-        color = new RGBColor(colorValue);
-        if (color.ok) {
-          jsonText["color"] = color.toHex();
+        var color;
+        var colorValue = gpmlNode.select('Graphics').attr('Color');
+        if (!!colorValue) {
+          color = new RGBColor(colorValue);
+          if (color.ok) {
+            jsonText["color"] = color.toHex();
+          }
+          else {
+            console.warn('Invalid color encountered. Setting color to black.');
+            jsonText["color"] = "#000000";
+          }
         }
-        else {
-          console.warn('Invalid color encountered. Setting color to black.');
-          jsonText["color"] = "#000000";
-        }
+        textCallbackOutside(jsonText);
       }
-
-      callback(jsonText);
+      else {
+        textCallbackOutside(null);
+      }
     }
     catch (e) {
       throw new Error("Error converting gpmlNode's text to renderable json: " + e.message);
