9608a0ec43226786ee5837c5b46858a40a164cb9
Anders Riutta <git@andersriutta.com>
2013-12-19 14:27:18 -0800

    added back CellularComponent to class attribute

--- src/js/pathvisiojs/data/gpml/shape.js
+++ src/js/pathvisiojs/data/gpml/shape.js
@@ -1,11 +1,43 @@
 pathvisiojs.data.gpml.shape = function(){
 
   var pathvisioDefaultStyleValues = {
-    'Rotation':'0.0',
-    'Color':'c0c0c0',
-    'FillColor':null,
-    'FontSize':10,
-    'FontWeight':null
+    'Shape':{
+      'Rotation':'0.0',
+      'Color':null,
+      'FillColor':null,
+      'FontSize':10,
+      'FontWeight':null,
+      'LineStyle':null,
+      'LineThickness':null,
+      'Cell':{
+        'Color':'c0c0c0',
+        'LineStyle':'Broken'
+      },
+      'Nucleus':{
+        'Color':'c0c0c0'
+      },
+      'EndoplasmicReticulum':{
+        'Color':'c0c0c0'
+      },
+      'GolgiApparatus':{
+        'Color':'c0c0c0'
+      },
+      'Mitochondria':{
+        'Color':'c0c0c0'
+      },
+      'SarcoplasmicReticulum':{
+        'Color':'c0c0c0'
+      },
+      'Organelle':{
+        'Color':'c0c0c0'
+      },
+      'Vesicle':{
+        'Color':'c0c0c0'
+      },
+      'ExtracellularRegion':{
+        'Color':'c0c0c0'
+      }
+    }
   }
 
   function toRenderableJson(gpmlShape, pathwayIri, callbackInside) {
@@ -14,33 +46,45 @@ pathvisiojs.data.gpml.shape = function(){
         jsonShape.nodeType = "Shape";
 
         var attributes = gpmlShape.selectAll('Attribute');
-        var cellularComponent;
+        var CellularComponent;
         if (attributes.length > 0) {
-          cellularComponent = attributes.filter(function(d, i) {
+          CellularComponent = attributes.filter(function(d, i) {
             return d3.select(this).attr('Key') === 'org.pathvisio.CellularComponentProperty' && d3.select(this).attr('Value') !== 'None';
           });
 
-          if (cellularComponent[0].length > 0) {
-            jsonShape.cellularComponent = cellularComponent.attr('Value');
+          if (CellularComponent[0].length > 0) {
+            jsonShape.CellularComponent = CellularComponent.attr('Value');
           }
         }
 
-        pathvisiojs.data.gpml.text.toRenderableJson(gpmlShape, pathvisioDefaultStyleValues, function(text) {
+        var thisPathvisioDefaultStyleValues;
+        if (!!jsonShape.CellularComponent) {
+          thisPathvisioDefaultStyleValues = pathvisiojs.utilities.collect(pathvisioDefaultStyleValues.Shape, pathvisioDefaultStyleValues.Shape[strcase.classCase(jsonShape.CellularComponent)]);
+        }
+        else {
+          thisPathvisioDefaultStyleValues = pathvisioDefaultStyleValues.Shape;
+        }
+
+        pathvisiojs.data.gpml.text.toRenderableJson(gpmlShape, thisPathvisioDefaultStyleValues, function(text) {
           if (!!text) {
             jsonShape.text = text;
           }
 
           jsonShape = pathvisiojs.data.gpml.setColorAsJson(jsonShape,
                         gpmlShape.select('Graphics').attr('Color'),
-                        pathvisioDefaultStyleValues.Color);
+                        thisPathvisioDefaultStyleValues.Color);
 
           jsonShape = pathvisiojs.data.gpml.node.setJsonBackgroundColor(jsonShape,
                         gpmlShape.select('Graphics').attr('FillColor'),
-                        pathvisioDefaultStyleValues.FillColor);
+                        thisPathvisioDefaultStyleValues.FillColor);
 
           jsonShape = pathvisiojs.data.gpml.entityNode.setJsonRotationValue(jsonShape,
                         gpmlShape.select('Graphics').attr('Rotation'),
-                        pathvisioDefaultStyleValues.Rotation);
+                        thisPathvisioDefaultStyleValues.Rotation);
+
+          jsonShape = pathvisiojs.data.gpml.setBorderStyleAsJson(jsonShape,
+                        gpmlShape.select('Graphics').attr('LineStyle'),
+                        thisPathvisioDefaultStyleValues.LineStyle);
 
           callbackInside(jsonShape);
         });
