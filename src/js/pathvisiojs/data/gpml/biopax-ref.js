ae5e78464c987b18fc5ae803beb52446fb3cf966
Anders Riutta <git@andersriutta.com>
2013-12-20 11:44:54 -0800

    split out biopax-ref getter code and removed unneeded duplicate file

--- src/js/pathvisiojs/data/gpml/biopax-ref.js
+++ src/js/pathvisiojs/data/gpml/biopax-ref.js
@@ -1,16 +1,27 @@
-pathvisiojs.pathway.biopaxRef = function(){
+pathvisiojs.data.gpml.biopaxRef = function(){
 
-  function toRenderableJson() {
+  function getAllAsRenderableJson(gpmlElement, callback) {
     try {
-
+      var tagName = gpmlElement[0][0].tagName;
+      var biopaxRefs = gpmlElement.selectAll(tagName + ' > BiopaxRef');
+      if (biopaxRefs[0].length > 0) {
+        publicationXrefs = [];
+        biopaxRefs.each(function() {
+          jsonPublicationXref = d3.select(this)[0][0].textContent;
+          publicationXrefs.push(jsonPublicationXref);
+        })
+        callback(publicationXrefs);
+      }
+      else {
+        callback(null);
+      }
     }
     catch (e) {
-      console.log("Error converting biopaxRefs to json: " + e.message);
-      return e;
+      throw new Error("Error converting biopaxRef to json: " + e.message);
     }
   }
 
   return {
-    toRenderableJson:toRenderableJson
+    getAllAsRenderableJson:getAllAsRenderableJson
   };
 }();
