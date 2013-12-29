2cf3bfaf9dfcfae50c9cd28db8cf696089cd381c
Anders Riutta <git@andersriutta.com>
2013-12-28 16:23:38 -0800

    labels on their way to being converted to new arch.

--- src/js/pathvisiojs/data/gpml/node/entity-node/label.js
+++ src/js/pathvisiojs/data/gpml/node/entity-node/label.js
@@ -1,7 +1,8 @@
 "use strict";
-pathvisiojs.data.gpml.element.node.entityNode.label = function(){
 
-  var pathvisioDefaultStyleValues = {
+pathvisiojs.data.gpml.element.node.entityNode.label = Object.create(pathvisiojs.data.gpml.element.node.entityNode);
+
+  var pathvisioDefaultStyleValuesLabel = {
     'Label':{
       'Rotation':null,
       'Color':null,
@@ -11,51 +12,38 @@ pathvisiojs.data.gpml.element.node.entityNode.label = function(){
     }
   }
 
-  function toRenderableJson(gpmlLabel, pathwayIri, callbackInside) {
-    /*
-    console.log('gpmlLabel');
-    console.log(gpmlLabel[0][0]);
-    console.log('pathwayIri');
-    console.log(pathwayIri);
-    console.log('callbackInside');
-    console.log(callbackInside);
-    //*/
-    try {
-      var jsonLabel = {};
-      jsonLabel.nodeType = "Label";
-      pathvisiojs.data.gpml.element.node.entityNode.toRenderableJson(gpmlLabel, jsonLabel, pathvisioDefaultStyleValues.Label, pathwayIri, function(jsonLabel) {
-        pathvisiojs.data.gpml.text.toRenderableJson(gpmlLabel, pathvisioDefaultStyleValues.Label, function(text) {
-          if (!!text) {
-            jsonLabel.text = text;
-          }
+pathvisiojs.data.gpml.element.node.entityNode.label.toRenderableJson = function(gpmlLabel, pathwayIri, callbackInside) {
+  /*
+  console.log('gpmlLabel');
+  console.log(gpmlLabel[0][0]);
+  console.log('pathwayIri');
+  console.log(pathwayIri);
+  console.log('callbackInside');
+  console.log(callbackInside);
+  //*/
+  var jsonLabel = {};
+  jsonLabel.nodeType = "Label";
+  pathvisiojs.data.gpml.element.node.entityNode.toRenderableJson(gpmlLabel, jsonLabel, pathvisioDefaultStyleValuesLabel.Label, pathwayIri, function(jsonLabel) {
+    pathvisiojs.data.gpml.text.toRenderableJson(gpmlLabel, pathvisioDefaultStyleValuesLabel.Label, function(text) {
+      if (!!text) {
+        jsonLabel.text = text;
+      }
 
-          jsonLabel = pathvisiojs.data.gpml.setColorAsJson(jsonLabel,
-                        gpmlLabel.select('Graphics').attr('Color'),
-                        pathvisioDefaultStyleValues.Label.Color);
+      jsonLabel = pathvisiojs.data.gpml.setColorAsJson(jsonLabel,
+                    gpmlLabel.select('Graphics').attr('Color'),
+                    pathvisioDefaultStyleValuesLabel.Label.Color);
 
-          jsonLabel = pathvisiojs.data.gpml.element.node.setJsonBackgroundColor(jsonLabel,
-                        gpmlLabel.select('Graphics').attr('FillColor'),
-                        pathvisioDefaultStyleValues.Label.FillColor);
+      jsonLabel = pathvisiojs.data.gpml.element.node.setJsonBackgroundColor(jsonLabel,
+                    gpmlLabel.select('Graphics').attr('FillColor'),
+                    pathvisioDefaultStyleValuesLabel.Label.FillColor);
 
-          var gpmlBackgroundColor = gpmlLabel.select('Graphics').attr('FillColor');
-          var jsonBackgroundColor = pathvisiojs.data.gpml.getColor(gpmlBackgroundColor, pathvisioDefaultStyleValues.Label.FillColor);
-          if (!!jsonBackgroundColor) {
-            jsonLabel.backgroundColor = jsonBackgroundColor;
-          }
+      var gpmlBackgroundColor = gpmlLabel.select('Graphics').attr('FillColor');
+      var jsonBackgroundColor = pathvisiojs.data.gpml.getColor(gpmlBackgroundColor, pathvisioDefaultStyleValuesLabel.Label.FillColor);
+      if (!!jsonBackgroundColor) {
+        jsonLabel.backgroundColor = jsonBackgroundColor;
+      }
 
-          callbackInside(jsonLabel);
-        });
-      });
-    }
-    catch (e) {
-      throw new Error("Error converting label to json: " + e.message);
-    }
-  }
-
-
-  return {
-    toRenderableJson:toRenderableJson
-  };
-}();
-
-
+      callbackInside(jsonLabel);
+    });
+  });
+}
