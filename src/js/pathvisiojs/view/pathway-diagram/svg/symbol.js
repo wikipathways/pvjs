e99b773feee56c64b91600e0bbe84186bfdf7eb5
Anders Riutta <git@andersriutta.com>
2014-01-03 14:08:29 -0800

    added definition to css file to make metabolites blue

--- src/js/pathvisiojs/view/pathway-diagram/svg/symbol.js
+++ src/js/pathvisiojs/view/pathway-diagram/svg/symbol.js
@@ -8,44 +8,28 @@ var svg;
 pathvisiojs.view.pathwayDiagram.svg.symbol = function(){
 
   var semanticNameToIdMapping = { 
-    'arrow':'shape-library-markers-arrow-svg',
-    'necessary-stimulation':'shape-library-markers-mim-necessary-stimulation-svg',
-    'binding':'shape-library-markers-mim-binding-svg',
-    'conversion':'shape-library-markers-mim-conversion-svg',
-    'stimulation':'shape-library-markers-mim-stimulation-svg',
-    'modification':'shape-library-markers-mim-modification-svg',
-    'catalysis':'shape-library-markers-mim-catalysis-svg',
-    'inhibition':'shape-library-markers-mim-inhibition-svg',
-    'cleavage':'shape-library-markers-mim-cleavage-svg',
-    'covalent-bond':'shape-library-markers-mim-covalent-bond-svg',
-    'transcription-translation':'shape-library-markers-mim-transcription-translation-svg',
-    'gap':'shape-library-markers-mim-gap-svg',
-    'inhibitory-activity':'shape-library-markers-t-bar-svg',
-    'unspecified':'shape-library-markers-none-svg',
-    'activity':'shape-library-markers-arrow-svg',
-    'mim-branching-left':'shape-library-markers-mim-branching-left-svg',
-    'mim-branching-right':'shape-library-markers-mim-branching-right-svg',
-    'mim-necessary-stimulation':'shape-library-markers-mim-necessary-stimulation-svg',
-    'mim-binding':'shape-library-markers-mim-binding-svg',
-    'mim-conversion':'shape-library-markers-mim-conversion-svg',
-    'mim-stimulation':'shape-library-markers-mim-stimulation-svg',
-    'mim-modification':'shape-library-markers-mim-modification-svg',
-    'mim-catalysis':'shape-library-markers-mim-catalysis-svg',
-    'mim-inhibition':'shape-library-markers-mim-inhibition-svg',
-    'mim-cleavage':'shape-library-markers-mim-cleavage-svg',
-    'mim-covalent-bond':'shape-library-markers-mim-covalent-bond-svg',
-    'mim-transcription-translation':'shape-library-markers-mim-transcription-translation-svg',
-    'mim-gap':'shape-library-markers-mim-gap-svg',
-    't-bar':'shape-library-markers-t-bar-svg',
-    'none':'shape-library-markers-none-svg'
+    'arc':'shape-library-symbols-arc-svg',
+    'brace':'shape-library-symbols-brace-svg',
+    'endoplasmic-reticulum':'shape-library-symbols-endoplasmic-reticulum-svg',
+    'golgi-apparatus':'shape-library-symbols-golgi-apparatus-svg',
+    'hexagon':'shape-library-symbols-hexagon-svg',
+    'mim-degradation':'shape-library-symbols-mim-degradation-svg',
+    'mitochondria':'shape-library-symbols-mitochondria-svg',
+    'oval':'shape-library-symbols-oval-svg',
+    'pentagon':'shape-library-symbols-pentagon-svg',
+    'rectangle':'shape-library-symbols-rectangle-svg',
+    'sarcoplasmic-reticulum':'shape-library-symbols-sarcoplasmic-reticulum-svg',
+    'triangle':'shape-library-symbols-triangle-svg',
+    'none':'shape-library-symbols-none-svg'
   };
 
-  function appendCustom(customSymbol, callback) {
+  function appendCustom(uniqueSymbolShapeUrl, callback) {
+    var symbolId = strcase.paramCase(uniqueSymbolShapeUrl)
     var defsSection = svg.select('defs');
-    var symbol = defsSection.select('#' + customSymbol.id);
+    var symbol = defsSection.select('#' + symbolId);
     if (!symbol[0][0]) {
       symbol = defsSection.append('symbol')
-      .attr('id', customSymbol.id)
+      .attr('id', symbolId)
       .attr('preserveAspectRatio', 'none');
     }
     else {
@@ -53,44 +37,31 @@ pathvisiojs.view.pathwayDiagram.svg.symbol = function(){
     }
 
     // ignoring non-svg symbols for now
-    //if (symbolType === 'svg') {
     if (1===1) {
-      d3.xml(customSymbol.url, "image/svg+xml", function(svgXml) {
+    //if (symbolType === 'svg') {
+      d3.xml(uniqueSymbolShapeUrl, "image/svg+xml", function(svgXml) {
         var shape = d3.select(svgXml.documentElement)
         var width = shape.attr('width');
         var height = shape.attr('height');
-        //var shapeClass = shape.attr('class');
-
         symbol.attr('viewBox', '0 0 ' + width + ' ' + height);
-
-          self.mySvgXml = svgXml;
-          var shapeSvg = d3.select(svgXml).select('svg');
-          var width = shapeSvg.attr('width');
-          var height = shapeSvg.attr('height');
-          symbol.attr('viewBox', '0 0 ' + width + ' ' + height);
-          var shapeChildren = shapeSvg[0][0].children;
-          d3.xml(customSymbol.url, "image/svg+xml", function(svgXml) {
-            self.mySvgXml = svgXml;
-            shapeSvg = d3.select(svgXml).select('svg');
-            var width = shapeSvg.attr('width');
-            var height = shapeSvg.attr('height');
-            symbol.attr('viewBox', '0 0 ' + width + ' ' + height);
-            //symbol.attr('class', shapeClass);
-            shapeChildren = shapeSvg[0][0].children;
-            do {
-              symbol[0][0].appendChild(shapeChildren[0]);
-            } while (shapeChildren.length > 0);
-          });
+        var shapeSvg = d3.select(svgXml).select('svg');
+        var width = shapeSvg.attr('width');
+        var height = shapeSvg.attr('height');
+        symbol.attr('viewBox', '0 0 ' + width + ' ' + height);
+        var shapeChildren = shapeSvg[0][0].children;
+        do {
+          symbol[0][0].appendChild(shapeChildren[0]);
+        } while (shapeChildren.length > 0);
         callback(null);
       });
     }
     else {
       img = document.createElement('img');
-      img.src = customSymbol.url;
+      img.src = uniqueSymbolShapeUrl;
       img.onload = function() {
         symbol.attr('viewBox', '0 0 ' + this.width + ' ' + this.height)
         dimensions = symbol.attr('viewBox').split(' ');
-        symbol.append('image').attr('xlink:xlink:href', customSymbol.url)
+        symbol.append('image').attr('xlink:xlink:href', uniqueSymbolShapeUrl)
         .attr('x', dimensions[0])
         .attr('y', dimensions[1])
         .attr('width', dimensions[2])
@@ -101,7 +72,7 @@ pathvisiojs.view.pathwayDiagram.svg.symbol = function(){
     }
 
     /*
-    symbol.append('object').attr('data', customSymbol.url)
+    symbol.append('object').attr('data', uniqueSymbolShapeUrl)
     .attr('x', dimensions[0])
     .attr('y', dimensions[1])
     .attr('width', dimensions[2])
@@ -120,7 +91,15 @@ pathvisiojs.view.pathwayDiagram.svg.symbol = function(){
     var dimensions = null;
     var dimensionSet = [];
 
-    async.each(customSymbols, appendCustom, function(err){
+    var uniqueSymbolShapeUrls = [];
+    customSymbols.forEach(function(customSymbol){
+      semanticNameToIdMapping[customSymbol.semanticName] = strcase.paramCase(customSymbol.url);
+      if (uniqueSymbolShapeUrls.indexOf(customSymbol.url) === -1) {
+        uniqueSymbolShapeUrls.push(customSymbol.url);
+      }
+    });
+
+    async.each(uniqueSymbolShapeUrls, appendCustom, function(err){
       // if any of the saves produced an error, err would equal that error
       callback(null);
     });
