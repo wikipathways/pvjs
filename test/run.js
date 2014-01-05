e4e124b0a7e82ad5b97cd7367116144463cc4236
Anders Riutta <git@andersriutta.com>
2014-01-05 13:58:38 -0800

    updated tests and minor updates to homepage

--- test/run.js
+++ test/run.js
@@ -53,7 +53,7 @@ exports.config = {
   //
   // Spec patterns are relative to the location of this config.
   specs: [
-    'e2e/shapes.js'
+    'e2e/*.js'
   ],
 
   // ----- Capabilities to be passed to the webdriver instance ----
