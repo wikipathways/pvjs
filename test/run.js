112dd18f8e105d9b8f401eb368cf9c63bb7ad089
Anders Riutta <git@andersriutta.com>
2014-01-13 15:49:50 -0800

    updated tests

--- test/run.js
+++ test/run.js
@@ -70,7 +70,7 @@ exports.config = {
   //
   // A base URL for your application under test. Calls to protractor.get()
   // with relative paths will be prepended with this.
-  baseUrl: 'http://127.0.0.1/~andersriutta/pathvisiojs/test/',
+  baseUrl: 'http://localhost:3000/',
 
   // Selector for the element housing the angular app - this defaults to
   // body, but is necessary if ng-app is on a descendant of <body>  
