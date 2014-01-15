366548f109970b9e7aec7bf7781cd5405e80e1bf
Alex Pico <apico@pointer.ucsf.edu>
2014-01-14 17:02:13 -0800

    updates to config files

---
+++ external-data/bridgedb/gpml.php
@@ -0,0 +1,23 @@
+<?php
+header("Content-Type: application/xml");
+header("Access-Control-Allow-Origin: *");
+  if (isset($_GET['id'])) {
+    $id = htmlspecialchars($_GET['id']);
+  }
+  else {
+    $id = 'WP4';
+  }
+
+  if (isset($_GET['rev'])) {
+    $rev = htmlspecialchars($_GET['rev']);
+  }
+  else {
+    $rev = 0;
+  }
+
+  $pathwayUrl = "http://www.wikipathways.org//wpi/wpi.php?action=downloadFile&type=gpml&pwTitle=Pathway:" . $id . "&rev=" . $rev;
+
+  // XML GPML (from either wikipathways.org REST API or local /test/gpml/ folder)
+    $pathway = simplexml_load_file($pathwayUrl);
+    echo $pathway->saveXML();
+?>
