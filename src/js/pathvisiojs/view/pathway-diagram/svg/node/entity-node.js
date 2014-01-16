95cb93ff5d81cec557edae988c0326dfb5fffc3c
Anders Riutta <git@andersriutta.com>
2014-01-15 16:34:47 -0800

    minor changes for tracking down bug with displaying xrefs

--- src/js/pathvisiojs/view/pathway-diagram/svg/node/entity-node.js
+++ src/js/pathvisiojs/view/pathway-diagram/svg/node/entity-node.js
@@ -1,6 +1,5 @@
 "use strict";
 pathvisiojs.view.pathwayDiagram.svg.node.EntityNode = function(){
-  //function render(viewport, data) {
   function render(args) {
     if (!args.container) {
       throw new Error('Container element not specified for this EntityNode.');
@@ -11,6 +10,8 @@ pathvisiojs.view.pathwayDiagram.svg.node.EntityNode = function(){
     if (!args.pathway) {
       throw new Error('Pathway not specified for this EntityNode. Pathway is needed for items like setting the Organism for DataNode annotations.');
     }
+    console.log('data');
+    console.log(args.data);
 
     pathvisiojs.view.pathwayDiagram.svg.node.render(args, function(nodeContainer) {
       nodeContainer.attr("class", function (d) {
@@ -27,8 +28,11 @@ pathvisiojs.view.pathwayDiagram.svg.node.EntityNode = function(){
         return cssClass;
       })
       if (!!args.data.DatasourceReference) {
+        console.log('args.data.DatasourceReference');
+        console.log(args.data.DatasourceReference);
         if (!!args.data.DatasourceReference.ID) {
           nodeContainer.on("click", function(d,i) {
+            console.log(d);
             pathvisiojs.view.annotation.xRef.render(args.pathway.Organism, d['DatasourceReference'].ID, d['DatasourceReference'].Database, d.text.line.join(' '), d.dataNodeType); //that's capital 'O' Organism from GPML vocab
           })
         }
