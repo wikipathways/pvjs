18d2ffccdef8036d1195af308b94e27a913a68df
Anders Riutta <git@andersriutta.com>
2014-01-21 22:03:34 -0800

    updated API for calling load()

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,7 @@ var pvjsSources = [
   'src/js/pathvisiojs/data/bridgedb/bridgedb.js',
   'src/js/pathvisiojs/data/bridgedb/data-sources.js',
   'src/js/pathvisiojs/data/biopax/biopax.js',
+  'src/js/pathvisiojs/data/pathvisiojs-json/pathvisiojs-json.js',
   'src/js/pathvisiojs/data/gpml/gpml.js',
   'src/js/pathvisiojs/data/gpml/element.js',
   'src/js/pathvisiojs/data/gpml/text.js',
