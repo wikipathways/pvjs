502c65d99a480b8787ef6b3482b277456925bc8e
Alexander Pico <apico@gladstone.ucsf.edu>
2014-01-22 14:16:44 -0800

    adds loadingGif config variable

--- config/wikipathways-github-io.js
+++ config/wikipathways-github-io.js
@@ -21,6 +21,10 @@ pathvisiojs.config = function() {
     return 'http://upload.wikimedia.org/wikipedia/commons/3/3b/Picture_Not_Yet_Available.png';
   }
 
+  var loadingGif = function() {
+    return '../src/img/loading.gif';
+  }
+
   var pngDiagramUriStub = function() {
     return 'http://www.wikipathways.org/wpi//wpi.php?action=downloadFile&type=png&pwTitle=Pathway:';
   }
