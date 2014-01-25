27b57ce7ee7e8b49eba53fe91d4092f2410edbd5
Anders Riutta <git@andersriutta.com>
2014-01-24 16:29:16 -0800

    undid data uri for diagram loading icon

--- src/js/pathvisiojs/view/annotation/x-ref.js
+++ src/js/pathvisiojs/view/annotation/x-ref.js
@@ -12,7 +12,7 @@ pathvisiojs.view.annotation.xRef = function(){
       var data = {
           "header": label,
           "description": desc,
-	  "listItems":[pathvisiojs.config.loadingGif()] 
+	  "listItems":[pathvisiojs.config.diagramLoadingIconUri()] 
         };
       pathvisiojs.view.annotation.render(data);
       //then retrieve the bridgedb data
