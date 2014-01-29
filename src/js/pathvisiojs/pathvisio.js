05930147a903735f6776ffb36a584d3af7b51232
Anders Riutta <git@andersriutta.com>
2014-01-28 16:56:13 -0800

    things are working

--- src/js/pathvisiojs/pathvisio.js
+++ src/js/pathvisiojs/pathvisio.js
@@ -1,31 +1,5 @@
 "use strict"
 
-// IE8 only allows console.log when Developer Tools is open. This will prevent errors
-// from showing up if I use console.log without DevTools being open.
-// from http://stackoverflow.com/questions/3326650/console-is-undefined-error-for-internet-explorer
-
-/**
- * Protect window.console method calls, e.g. console is not defined on IE
- * unless dev tools are open, and IE doesn't define console.debug
- */
-(function() {
- if (!window.console) {
-   window.console = {};
- }
- // union of Chrome, FF, IE, and Safari console methods
- var m = [
- "log", "info", "warn", "error", "debug", "trace", "dir", "group",
- "groupCollapsed", "groupEnd", "time", "timeEnd", "profile", "profileEnd",
- "dirxml", "assert", "count", "markTimeline", "timeStamp", "clear"
- ];
- // define undefined methods as noops to prevent errors
- for (var i = 0; i < m.length; i++) {
-   if (!window.console[m[i]]) {
-   window.console[m[i]] = function() {};
-   }    
- } 
-})();
-
 var pathvisiojs = function(){
 
   var svg, pathway, args;
