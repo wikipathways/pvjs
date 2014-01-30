8f54d6232cad1d16257614104c7a900a5e12e0bb
Anders Riutta <git@andersriutta.com>
2014-01-29 18:22:18 -0800

    update script to count pathway element numbers

--- test/scripts/count-gpml-elements.py
+++ test/scripts/count-gpml-elements.py
@@ -1,7 +1,24 @@
+import StringIO, json
 from lxml import etree
-import StringIO
-tree = etree.parse("http://localhost:3000/test/data/protocol/elbows.gpml.xml")
-species = tree.xpath('/foo:Pathway/@Organism', namespaces={'foo': 'http://pathvisio.org/GPML/2013a'})
-dataNodes = tree.xpath('//foo:DataNode', namespaces={'foo': 'http://pathvisio.org/GPML/2013a'})
-print(species)
-print( len( dataNodes ) )
+from json import dumps, load
+
+data = {}
+
+pathways = ['anchors', 'citations', 'curves', 'elbows', 'fill-and-stroke', 'gpml-data-nodes', 'gpml-labels', 'graphical-lines', 'groups', 'interactions', 'shapes', 'size-and-proportion', 'text', 'z-index']
+for pathway in pathways:
+	data[pathway] = {}
+	tree = etree.parse("http://localhost:3000/test/data/protocol/" + pathway + ".gpml.xml")
+	elementTags = ['DataNode', 'Label', 'Shape', 'Interaction', 'GraphicalLine', 'Group']
+	for elementTag in elementTags:
+		elements = tree.xpath('//foo:' + elementTag, namespaces={'foo': 'http://pathvisio.org/GPML/2013a'})
+		elementCount = len(elements)
+		data[pathway][elementTag + 'Count'] = elementCount
+
+	dataNodeTypes = ['GeneProduct', 'Metabolite', 'Protein', 'Unknown', 'Pathway']
+	for dataNodeType in dataNodeTypes:
+		dataNodeTypes = tree.xpath('//foo:DataNode[@Type="' + dataNodeType + '"]', namespaces={'foo': 'http://pathvisio.org/GPML/2013a'})
+		dataNodeTypeCount = len(dataNodeTypes)
+		data[pathway][dataNodeType + 'Count'] = dataNodeTypeCount
+
+with open('../data/protocol/counts.json', 'w') as outfile:
+	json.dump(data, outfile, indent = 4)
