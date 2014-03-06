9f18ec243b64af3c3672475a4d85faf5eb35348b
Anders Riutta <git@andersriutta.com>
2014-03-06 13:07:57 -0800

    refactor ...gpml.element to self-executing function form

--- src/js/pathvisiojs/data/gpml/element.js
+++ src/js/pathvisiojs/data/gpml/element.js
@@ -1,152 +1,368 @@
-'use strict';
+pathvisiojs.data.gpml.element = function(){
+  // includes all GPML elements. Is parent of node and edge.
+  'use strict';
 
-// includes all GPML elements. Is parent of node and edge.
-
-pathvisiojs.data.gpml.element = {};
-
-
-var pathvisioDefaultStyleValues = {
-  'FontSize':{
-    'Type':"FontSize",
-    'Value':10
-  }
-}
-
-pathvisiojs.data.gpml.element.gpmlColorToCssColor = function(gpmlColor, pathvisioDefault) {
-  var color;
-  if (gpmlColor !== pathvisioDefault) {
-    if (!!gpmlColor) {
-      color = new RGBColor(gpmlColor);
-      if (color.ok) {
-        return color.toHex();
+  var gpmlColorToCssColor = function(gpmlColor, pathvisioDefault) {
+    var color;
+    if (gpmlColor !== pathvisioDefault) {
+      if (!!gpmlColor) {
+        color = new RGBColor(gpmlColor);
+        if (color.ok) {
+          return color.toHex();
+        }
+        else {
+          return 'black';
+        }
       }
       else {
         return 'black';
       }
     }
     else {
-      return 'black';
+      return null;
     }
-  }
-  else {
-    return null;
-  }
-}
+  };
 
-pathvisiojs.data.gpml.element.setColorAsJson = function(jsonElement, currentGpmlColorValue, defaultGpmlColorValue) {
-  var jsonColor;
-  if (currentGpmlColorValue !== defaultGpmlColorValue) {
-    jsonColor = gpmlColorToCssColor(currentGpmlColorValue, defaultGpmlColorValue);
-    jsonElement.color = jsonColor;
-    jsonElement.borderColor = jsonColor;
-    if (jsonElement.hasOwnProperty('text')) {
-      jsonElement.text.color = jsonColor;
+  var setColorAsJson = function(jsonElement, currentGpmlColorValue, defaultGpmlColorValue) {
+    var jsonColor;
+    if (currentGpmlColorValue !== defaultGpmlColorValue) {
+      jsonColor = gpmlColorToCssColor(currentGpmlColorValue, defaultGpmlColorValue);
+      jsonElement.color = jsonColor;
+      jsonElement.borderColor = jsonColor;
+      if (jsonElement.hasOwnProperty('text')) {
+        jsonElement.text.color = jsonColor;
+      }
     }
-  }
-  return jsonElement;
-}
+    return jsonElement;
+  };
 
+  // TODO can we delete this function?
 
-pathvisiojs.data.gpml.element.setColorAsJson = function(jsonElement, currentGpmlColorValue, defaultGpmlColorValue) {
-  var jsonColor;
-  if (currentGpmlColorValue !== defaultGpmlColorValue) {
-    jsonColor = gpmlColorToCssColor(currentGpmlColorValue, defaultGpmlColorValue);
-    jsonElement.color = jsonColor;
-    jsonElement.borderColor = jsonColor;
-    if (jsonElement.hasOwnProperty('text')) {
-      jsonElement.text.color = jsonColor;
-    }
-  }
-  return jsonElement;
-}
+  var getLineStyle = function(gpmlElement) {
+    var LineStyle, attributes; 
+    var graphics = gpmlElement.select('Graphics');
+    if (!!graphics) {
+      LineStyle = graphics.attr('LineStyle'); 
+      if (!!LineStyle) {
+        return LineStyle;
+      }
+      else {
 
-// TODO can we delete this function?
+        // As currently specified, a given element can only have one LineStyle.
+        // This one LineStyle can be solid, dashed (broken) or double.
+        // If no value is specified in GPML for LineStyle, then we need to check
+        // for whether the element has LineStyle of double.
 
-pathvisiojs.data.gpml.element.getLineStyle = function(gpmlElement) {
-  var LineStyle, attributes; 
-  var graphics = gpmlElement.select('Graphics');
-  if (!!graphics) {
-    LineStyle = graphics.attr('LineStyle'); 
-    if (!!LineStyle) {
-      return LineStyle;
-    }
-    else {
+        attributes = gpmlElement.selectAll('Attribute');
+        if (attributes.length > 0) {
+          LineStyle = attributes.filter(function(d, i) {
+            return d3.select(this).attr('Key') === 'org.pathvisiojs.DoubleLineProperty' && d3.select(this).attr('Value') === 'Double';
+          });
 
-      // As currently specified, a given element can only have one LineStyle.
-      // This one LineStyle can be solid, dashed (broken) or double.
-      // If no value is specified in GPML for LineStyle, then we need to check
-      // for whether the element has LineStyle of double.
-
-      attributes = gpmlElement.selectAll('Attribute');
-      if (attributes.length > 0) {
-        LineStyle = attributes.filter(function(d, i) {
-          return d3.select(this).attr('Key') === 'org.pathvisiojs.DoubleLineProperty' && d3.select(this).attr('Value') === 'Double';
-        });
-
-        if (LineStyle[0].length > 0) {
-          return 'double';
+          if (LineStyle[0].length > 0) {
+            return 'double';
+          }
+          else {
+            return null;
+          }
         }
         else {
           return null;
         }
       }
-      else {
-        return null;
-      }
     }
-  }
-}
-
-pathvisiojs.data.gpml.element.getBorderStyle = function(gpmlLineStyle, pathvisioDefault) {
-
-  // Double-lined EntityNodes will be handled by using a symbol with double lines.
-  // Double-lined edges will be rendered as single-lined, solid edges, because we
-  // shouldn't need double-lined edges other than for cell walls/membranes, which
-  // should be symbols. Any double-lined edges are curation issues.
-
-  var lineStyleToBorderStyleMapping = {
-    'Solid':'solid',
-    'Double':'solid',
-    'Broken':'dashed'
   };
-  var borderStyle;
-  if (gpmlLineStyle !== pathvisioDefault) {
-    if (!!gpmlLineStyle) {
-      borderStyle = lineStyleToBorderStyleMapping[gpmlLineStyle];
-      if (borderStyle) {
-        return borderStyle;
+
+  var getBorderStyle = function(gpmlLineStyle, pathvisioDefault) {
+
+    // Double-lined EntityNodes will be handled by using a symbol with double lines.
+    // Double-lined edges will be rendered as single-lined, solid edges, because we
+    // shouldn't need double-lined edges other than for cell walls/membranes, which
+    // should be symbols. Any double-lined edges are curation issues.
+
+    var lineStyleToBorderStyleMapping = {
+      'Solid':'solid',
+      'Double':'solid',
+      'Broken':'dashed'
+    };
+    var borderStyle;
+    if (gpmlLineStyle !== pathvisioDefault) {
+      if (!!gpmlLineStyle) {
+        borderStyle = lineStyleToBorderStyleMapping[gpmlLineStyle];
+        if (borderStyle) {
+          return borderStyle;
+        }
+        else {
+          console.warn('LineStyle "' + gpmlLineStyle + '" does not have a corresponding borderStyle. Using "solid"');
+          return 'solid';
+        }
       }
       else {
-        console.warn('LineStyle "' + gpmlLineStyle + '" does not have a corresponding borderStyle. Using "solid"');
         return 'solid';
       }
     }
     else {
-      return 'solid';
+
+      // TODO use code to actually get the default
+
+      return 'whatever the default value is';
     }
-  }
-  else {
+  };
 
-    // TODO use code to actually get the default
+  var setBorderStyleAsJson = function(jsonElement, currentGpmlLineStyleValue, defaultGpmlLineStyleValue) {
+    var borderStyle;
 
-    return 'whatever the default value is';
-  }
-}
+    // this check happens twice because it doesn't make sense to have getBorderStyle() tell us
+    // whether it has returned the default value, and we need to know whether we are using the
+    // default here.
 
-pathvisiojs.data.gpml.element.setBorderStyleAsJson = function(jsonElement, currentGpmlLineStyleValue, defaultGpmlLineStyleValue) {
-  var borderStyle;
+    if (currentGpmlLineStyleValue !== defaultGpmlLineStyleValue) {
+      borderStyle = getBorderStyle(currentGpmlLineStyleValue, defaultGpmlLineStyleValue);
+      jsonElement.borderStyle = borderStyle;
+    }
+    return jsonElement;
+  };
 
-  // this check happens twice because it doesn't make sense to have getBorderStyle() tell us
-  // whether it has returned the default value, and we need to know whether we are using the
-  // default here.
+  var toPvjson = function(gpmlElement, jsonElement, elementCallback) {
+    jsonElement["@type"] = jsonElement["@type"] || [];
+    jsonElement["@type"].push("element");
 
-  if (currentGpmlLineStyleValue !== defaultGpmlLineStyleValue) {
-    borderStyle = getBorderStyle(currentGpmlLineStyleValue, defaultGpmlLineStyleValue);
-    jsonElement.borderStyle = borderStyle;
-  }
-  return jsonElement;
-}
+    pathvisiojs.data.gpml.biopaxRef.getAllAsPvjson(gpmlElement, function(publicationXrefs) {
+      if (!!publicationXrefs) {
+        jsonElement.PublicationXref = publicationXrefs;
+      }
+      elementCallback(jsonElement);
+    });
+  };
 
+  //*
+  var toPvjsonNew = function(gpmlSelection, elementSelection, pvjsonElement, callback) {
+      var parentElement,
+      pvjsonText = {},
+      attribute,
+      i,
+      graphics = elementSelection.select('Graphics')[0][0],
+      pvjsonHeight,
+      pvjsonWidth,
+      pvjsonStrokeWidth,
+      gpmlShapeType,
+      pvjsonShape,
+      pvjsonZIndex,
+      pvjsonRelX,
+      pvjsonRelY,
+      pvjsonX,
+      pvjsonY;
+
+    var attributeDependencyOrder = [
+      'ShapeType',
+      'FillColor',
+      'Color',
+      'LineThickness',
+      'Width',
+      'Height',
+      'RelX',
+      'RelY',
+      'CenterX',
+      'CenterY'
+    ];
+
+    var gpmlToPvjsonConverter = {
+      LineStyle: function(gpmlLineStyleValue){
+        // LineStyle of "Double" is set using a new element named "Attribute"
+        // but it's not actually an attribute on the Graphics element.
+        // It is handled when determining ShapeType below.
+        var pvjsonStrokeDasharray;
+        // TODO hard-coding these here is not the most maintainable
+        if (gpmlLineStyleValue === 'Broken') {
+          pvjsonStrokeDasharray = '5,3';
+        }
+        else {
+          pvjsonStrokeDasharray = '9999999999999999999999999';
+        }
+        pvjsonElement.strokeDasharray = pvjsonStrokeDasharray;
+        return pvjsonStrokeDasharray;
+      },
+      ShapeType: function(gpmlShapeTypeValue){
+        var doubleProperty;
+        gpmlShapeType = gpmlShapeTypeValue;
+        if (gpmlShapeType === 'None') {
+          pvjsonShape = 'rectangle';
+        }
+        else {
+          pvjsonShape = strcase.paramCase(gpmlShapeType);
+          doubleProperty = elementSelection.select('[Key="org.pathvisio.DoubleLineProperty"]')[0][0];
+          if (!!doubleProperty) {
+            pvjsonShape += '-double';
+          }
+        }
+        pvjsonElement.shape = pvjsonShape;
+        return pvjsonShape;
+      },
+      FillColor: function(gpmlFillColorValue){
+        var cssColor = gpmlColorToCssColor(gpmlFillColorValue);
+        if (gpmlShapeType.toLowerCase() !== 'none') {
+          pvjsonElement.fill = cssColor;
+        }
+        else {
+          pvjsonElement.fill = 'transparent';
+        }
+      },
+      Color: function(gpmlColorValue){
+        var cssColor = gpmlColorToCssColor(gpmlColorValue);
+        if (gpmlShapeType !== 'None') {
+          pvjsonElement.stroke = cssColor;
+        }
+        else {
+          pvjsonElement.stroke = 'transparent';
+        }
+        pvjsonText.fill = cssColor;
+      },
+      FontSize: function(gpmlFontSizeValue){
+        var cssFontSize = gpmlFontSizeValue;
+        pvjsonText.fontSize = cssFontSize;
+      },
+      FontWeight: function(gpmlFontWeightValue){
+        var cssFontWeight = gpmlFontWeightValue.toLowerCase();
+        pvjsonText.fontWeight = cssFontWeight;
+      },
+      Rotation: function(gpmlRotationValue) {
+        // GPML can hold a rotation value for State elements in an element named "Attribute" like this:
+        // Key="org.pathvisio.core.StateRotation"
+        // From discussion with AP and KH, we've decided to ignore this value, because we don't actually want States to be rotated.
+        gpmlRotationValue = parseFloat(gpmlRotationValue);
+        var pvjsonRotation = gpmlRotationValue * 180/Math.PI; //converting from radians to degrees
+        // TODO how do we want to store this value?
+        pvjsonElement.rotation = pvjsonRotation;
+        pvjsonText.rotation = pvjsonRotation;
+        return pvjsonRotation;
+      },
+      LineThickness: function(gpmlLineThicknessValue) {
+        pvjsonStrokeWidth = parseFloat(gpmlLineThicknessValue);
+        pvjsonElement.strokeWidth = pvjsonStrokeWidth;
+        return pvjsonStrokeWidth;
+      },
+      Width: function(gpmlWidthValue) {
+        console.log(gpmlWidthValue);
+        gpmlWidthValue = parseFloat(gpmlWidthValue);
+        pvjsonWidth = gpmlWidthValue + pvjsonStrokeWidth;
+        pvjsonElement.width = pvjsonWidth;
+        pvjsonText.width = pvjsonWidth;
+        return pvjsonWidth;
+      },
+      Height: function(gpmlHeightValue) {
+        gpmlHeightValue = parseFloat(gpmlHeightValue);
+        pvjsonHeight = gpmlHeightValue + pvjsonStrokeWidth;
+        pvjsonElement.height = pvjsonHeight;
+        pvjsonText.height = pvjsonHeight;
+        return pvjsonHeight;
+      },
+      CenterX: function(gpmlCenterXValue) {
+        gpmlCenterXValue = parseFloat(gpmlCenterXValue);
+        pvjsonX = gpmlCenterXValue - pvjsonWidth/2;
+        pvjsonElement.x = pvjsonX;
+        pvjsonText.x = pvjsonX;
+        return pvjsonX;
+      },
+      CenterY: function(gpmlCenterYValue) {
+        gpmlCenterYValue = parseFloat(gpmlCenterYValue);
+        pvjsonY = gpmlCenterYValue - pvjsonHeight/2;
+        pvjsonElement.y = pvjsonY;
+        pvjsonText.y = pvjsonY;
+        return pvjsonY;
+      },
+      RelX: function(gpmlRelXValue) {
+        pvjsonRelX = parseFloat(gpmlRelXValue);
+        pvjsonElement.relX = pvjsonRelX;
+        pvjsonText.relX = pvjsonRelX;
+        parentElement = gpmlSelection.select('[GraphId=' + elementSelection.attr('GraphId') + ']');
+        var parentCenterX = parentElement.attr('CenterX');
+        var parentWidth = parentElement.attr('Width');
+        var gpmlCenterXValue = parentCenterX + gpmlRelXValue * parentWidth/2;
+        pvjsonX = gpmlCenterXValue - pvjsonWidth/2;
+        pvjsonElement.x = pvjsonX;
+        pvjsonText.x = pvjsonX;
+        return pvjsonX;
+      },
+      RelY: function(gpmlRelYValue) {
+        gpmlRelYValue = parseFloat(gpmlRelYValue);
+        pvjsonElement.relY = gpmlRelYValue;
+        pvjsonText.relY = gpmlRelYValue;
+        var parentCenterY = parentElement.attr('CenterY');
+        var parentHeight = parentElement.attr('Height');
+        var gpmlCenterYValue = parentCenterY + gpmlRelYValue * parentHeight/2;
+        pvjsonY = gpmlCenterYValue - pvjsonHeight/2;
+        pvjsonElement.y = pvjsonY;
+        pvjsonText.y = pvjsonY;
+        return pvjsonY;
+      },
+      ZOrder: function(gpmlZOrderValue) {
+        pvjsonZIndex = parseFloat(gpmlZOrderValue);
+        pvjsonElement.zIndex = pvjsonZIndex;
+        pvjsonText.zIndex = pvjsonZIndex;
+        return pvjsonZIndex;
+      }
+    };
+
+    var attributeName, attributeListItem, attributeListItemName, attributeList = [];
+    if (!!graphics) {
+      for (i = 0; i < graphics.attributes.length; i++) {
+        attribute = graphics.attributes[i];
+        attributeName = attribute.name;
+        attributeListItem = {
+          name: attributeName,
+          value: attribute.value,
+          dependencyOrder: attributeDependencyOrder.indexOf(attributeName),
+        };
+        attributeList.push(attributeListItem);
+      }
+      attributeList.sort(function(a, b) {
+        return a.dependencyOrder - b.dependencyOrder;
+      });
+      attributeList.forEach(function(attributeListItem){
+        attributeListItemName = attributeListItem.name;
+        if (gpmlToPvjsonConverter.hasOwnProperty(attributeListItemName)) {
+          gpmlToPvjsonConverter[attributeListItemName](attributeListItem.value);
+        }
+        else {
+          console.warn('Pathvisiojs has no handler for attribute "' + attributeListItemName + '"');
+          attributeListItemName = strcase.paramCase(attributeListItemName);
+          pvjsonElement[attributeListItemName] = attributeListItem.value;
+        }
+      });
+    }
+
+    function gpmlColorToCssColor(gpmlColor) {
+      var color;
+      if (gpmlColor.toLowerCase() === 'transparent') {
+        return 'transparent';
+      }
+      else {
+        color = new RGBColor(gpmlColor);
+        if (color.ok) {
+          return color.toHex();
+        }
+        else {
+          console.warn('Could not convert GPML Color value of "' + gpmlColor + '" to a valid CSS color. Using "#c0c0c0" as a fallback.');
+          return '#c0c0c0';
+        }
+      }
+    }
+    callback(pvjsonElement, pvjsonText);
+  };
+  //*/
+
+  return {
+    toPvjson:toPvjson,
+    toPvjsonNew:toPvjsonNew,
+    gpmlColorToCssColor:gpmlColorToCssColor,
+    setColorAsJson:setColorAsJson,
+    getLineStyle:getLineStyle,
+    getBorderStyle:getBorderStyle,
+    setBorderStyleAsJson:setBorderStyleAsJson
+  };
+}();
+
+/*
 // set default values. "swing" refers to PathVisio-Java.
 pathvisiojs.data.gpml.element.color = {};
 pathvisiojs.data.gpml.element.color.swing = '000000';
@@ -171,28 +387,6 @@ pathvisiojs.data.gpml.element.fontWeight.gpml = null;
 pathvisiojs.data.gpml.element.fontName = {};
 pathvisiojs.data.gpml.element.fontName.swing = 'Arial';
 pathvisiojs.data.gpml.element.fontName.gpml = null;
+//*/
   
-pathvisiojs.data.gpml.element.toPvjson = function(gpmlElement, jsonElement, elementCallback) {
-  jsonElement["@type"] = jsonElement["@type"] || [];
-  jsonElement["@type"].push("element");
 
-  pathvisiojs.data.gpml.biopaxRef.getAllAsPvjson(gpmlElement, function(publicationXrefs) {
-    if (!!publicationXrefs) {
-      jsonElement.PublicationXref = publicationXrefs;
-    }
-    elementCallback(jsonElement);
-  });
-
-  /*
-     var graphics = gpmlElement.select('Graphics'),
-     zIndex,
-     borderWidth;
-     if (graphics[0].length > 0) {
-     zIndex = graphics.attr('ZOrder') || 1;
-     jsonElement.zIndex = parseFloat(borderWidth);
-
-     borderWidth = graphics.attr('LineThickness') || 1;
-     jsonElement.borderWidth = parseFloat(borderWidth);
-     }
-  //*/
-};
