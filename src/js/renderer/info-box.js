583bb8e9a551e622dfa6bc697fe313c1e163a11b
Anders Riutta <git@andersriutta.com>
2014-04-07 14:54:58 -0700

    moved annotations out and removed unneeded old code. it appears to work.

---
+++ src/js/renderer/info-box.js
@@ -0,0 +1,64 @@
+pathvisiojs.renderer.infoBox = function(){
+  'use strict';
+    
+  function render(viewport, pathway) {
+    if (!viewport || !pathway) {
+      return console.warn('Error: Missing input parameters.');
+    }
+
+    // Although gpml has x and y values for infobox, we have decided to ignore them and always set it in the upper left.
+
+    var infoBox = [];
+    if (pathway.hasOwnProperty('Name')) {
+      infoBox.push({'key':'Title', 'value':pathway.Name});
+    }
+
+    if (pathway.hasOwnProperty('License')) {
+      infoBox.push({'key':'Availability', 'value':pathway.License});
+    }
+
+    if (pathway.hasOwnProperty('LastModified')) {
+      infoBox.push({'key':'Last modified', 'value':pathway.LastModified});
+    }
+
+    if (pathway.hasOwnProperty('Organism')) {
+      infoBox.push({'key':'Organism', 'value':pathway.Organism});
+    }
+
+    /*
+    if (pathway.hasOwnProperty('PublicationXref')) {
+      pathvisiojs.view.pathwayDiagram.svg.publicationXref.getPublicationXrefString(pathway, pathway.PublicationXref, function(publicationXrefString) {
+        infoBox.push({'key':'Citation(s)', 'value':publicationXrefString});
+      })
+    }
+    //*/
+
+    var infoBox = viewport.selectAll("g.info-box")
+    .data([infoBox])
+    .enter()
+    .append("g")
+    .attr("id", function (d,i) {return "info-box-" + i; })
+    .attr("class", "text-area info-box");
+
+    var infoBoxItems = infoBox.selectAll("text")
+    .data(function(d) {return d;})
+    .enter()
+    .append("text")
+    .attr("id", function (d,i) {return "info-box-text" + i; })
+    .attr("class", "item")
+    .attr("x", 0)
+    .attr("y", function(d,i) {return 14 + 14 * i; });
+
+    var infoBoxPropertyName = infoBoxItems.append("tspan")
+    .attr("class", "info-box-item-property-name")
+    .text(function (d) {return d.key + ': ';});
+
+    var infoBoxProperty = infoBoxItems.append("tspan")
+    .attr("class", "info-box-item-property-value")
+    .text(function (d) {return d.value;});
+  }
+
+  return {
+    render:render
+  };
+}();
