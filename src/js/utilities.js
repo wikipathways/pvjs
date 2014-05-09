0a5bedb6f7a0be4fb96d6c1a0ccf44a12dc548da
bumbu <bmbalex@gmail.com>
2014-05-09 19:11:41 +0200

    isElement matches SVGElement in ie

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -381,7 +381,7 @@ var Utils = {
    */
 , isElement: function(o){
     return (
-      typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
+      typeof HTMLElement === "object" ? (o instanceof HTMLElement || o instanceof SVGElement || o instanceof SVGSVGElement) : //DOM2
       o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName==="string"
     );
   }
