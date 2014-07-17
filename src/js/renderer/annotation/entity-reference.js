0bda331bce20ce845d24c6f158203525d6725336
Anders Riutta <git@andersriutta.com>
2014-07-16 17:52:04 -0700

    linkouts appear to be working well. Added linkout functionality for pathways, closing issue #252, and for cellular components.

--- src/js/renderer/annotation/entity-reference.js
+++ src/js/renderer/annotation/entity-reference.js
@@ -1,24 +1,67 @@
 var Annotation = require('./annotation.js')
   , Bridgedb = require('bridgedbjs')
+  , jsonld = require('jsonld')
   ;
 
 module.exports = function(){
   'use strict';
 
   function render(pvjs, args) {
+    var xrefs = args.xrefs;
+    var metadata = args.metadata;
     // TODO bumbu hardcoded
     var diagramLoadingIconUri = 'http://www.wikipathways.org/wpi/extensions/PathwayViewer/img/loading.gif'
     var preloadedData = {
-      "header": args.label,
-      "description": args.description,
-      "listItems":[diagramLoadingIconUri]
+      'header': metadata.label,
+      'description': metadata.description,
+      'listItems':[diagramLoadingIconUri]
     };
     Annotation.render(pvjs, preloadedData);
 
-    //then retrieve the bridgedb data
-    var xRefData = Bridgedb.getXrefsNestedForDisplay(args, function(err, bridgedbData) {
-      Annotation.render(pvjs, bridgedbData);
-    });
+    if (xrefs.id.indexOf('bridgedb.org') > -1) {
+      // dereference the BridgeDB IRI to get multiple xrefs
+      var bridgedbArgs = metadata;
+      bridgedbArgs.bridgedbUri = xrefs.id;
+      var xRefData = Bridgedb.getXrefsNestedForDisplay(bridgedbArgs, function(err, bridgedbData) {
+        Annotation.render(pvjs, bridgedbData);
+      });
+    } else {
+      console.log('need to display an xref');
+      console.log(xrefs);
+      console.log('pvjs');
+      console.log(pvjs);
+      self.mypvjs = pvjs;
+
+      var xrefWithContext = {'@context':pvjs.sourceData.pvjson['@context'], '@graph':{entityReference:xrefs.id}}
+      jsonld.expand(xrefWithContext, function(err, expandedXref) {
+        console.log('err');
+        console.log(err);
+        console.log('expandedXref');
+        console.log(expandedXref);
+        var xrefIri = expandedXref[0]['http://www.biopax.org/release/biopax-level3.owl#entityReference'][0]['@id'];
+        console.log('xrefIri');
+        console.log(xrefIri);
+
+        var directLinkData;
+        if (xrefIri.indexOf('wikipathways') > -1 && xrefIri.search(/WP\d{1,5}(\_r\d+)?$/) > -1) {
+          var wikipathwaysId = xrefIri.match(/WP\d{1,5}(\_r\d+)?$/)[0];
+          directLinkData = {
+            'header': metadata.label,
+            'description': metadata.description,
+            'listItems':[{key:'WikiPathways', values:[ { text: wikipathwaysId, uri: xrefIri }]}]
+          };
+        } else if (xrefIri.search(/GO:\d{7}$/) > -1) { // because this is none of the types that BridgeDB handles, I'm only expecting GO Cell Ontology terms here
+          var goId = xrefIri.match(/GO:\d{7}$/)[0];
+          directLinkData = {
+            'header': xrefs.id,
+            'description': '',
+            'listItems':[{key:'GO Cellular Component Ontology', values:[ { text: goId, uri: xrefIri }]}]
+          };
+        }
+
+        Annotation.render(pvjs, directLinkData);
+      });
+    }
   }
 
   return {
