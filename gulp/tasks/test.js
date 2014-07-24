7b0a1145acf23af98122139593bc042d6585ad9d
Anders Riutta <git@andersriutta.com>
2014-07-24 12:01:51 -0700

    got selenium/wd/mocha/gulp to all work together

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -6,23 +6,65 @@
 // see also: https://github.com/mllrsohn/gulp-protractor/issues/12
 //
 // 1) launch selenium standalone server: https://github.com/daaku/nodejs-selenium-launcher
-// 2) Use one the the options for implementing the Selenium WebDriver Wire Protocol
+// 2) Use one of the the options for implementing the Selenium WebDriver Wire Protocol
     // [ sode ](https://github.com/LearnBoost/soda)
     // [ wd ](https://github.com/admc/wd)
     // [ nightwatch ](http://nightwatchjs.org/)
+// 3) Run tests with mocha
+    //
 //
 
+var gulp = require('gulp');
 var seleniumLauncher = require('selenium-launcher');
-seleniumLauncher(function(er, selenium) {
-  // selenium is running
-  // selenium.host / selenium.port are available
-  // selenium is a child process, so you can do selenium.kill()
-  if (er) {
-    selenium.kill();
-  }
+var mocha = require('gulp-spawn-mocha');
 
-  require('../../tests/demo.js');
+gulp.task('test', function () {
+  seleniumLauncher(function(er, selenium) {
+    // selenium is running
+    // selenium.host / selenium.port are available
+    // selenium is a child process, so you can do selenium.kill()
+    if (er) {
+      selenium.kill();
+    }
 
+
+
+
+
+    return test().on('error', function (e) {
+      throw e;
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+  //require('../../tests/demo.js');
+
+  /*
   selenium.kill();
   console.log('bye');
+  //*/
 });
+
+/*
+gulp.task('default', function () {
+  gulp.watch('{lib,test}/*', test);
+  test();
+});
+//*/
+
+function test() {
+  return gulp.src(['./test/e2e/dev.js'], {read: false}).pipe(mocha({
+    r: './test/wd-test-config.js',
+    R: 'spec',
+    c: true,
+    debug: true
+  })).on('error', console.warn.bind(console));
+}
