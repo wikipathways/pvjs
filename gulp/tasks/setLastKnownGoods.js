0bcb998e873e6ad1ea9f7993979b420e4f5bdb70
Anders Riutta <git@andersriutta.com>
2014-08-05 17:27:41 -0700

    we are now generating the hashes required for comparing test screenshots vs. last known good screenshots

--- gulp/tasks/setLastKnownGoods.js
+++ gulp/tasks/setLastKnownGoods.js
@@ -4,23 +4,37 @@ var gulp = require('gulp')
   , crypto = require('crypto')
   , fs   = require('fs')
   , highland = require('highland')
+  , os   = require('os')
   , path = require('path')
   , through = require('through')
   ;
 
 gulp.task('setLastKnownGoods', function () {
-  var protocolTestResultsDirectory = './tmp/protocol/';
+  var protocolTestResultsDirectory = './tmp/protocol/'
+    , protocolTestLastKnownGoodsDirectory = './test/last-known-goods/protocol/'
+    ;
 
-  var imageHashStream;
   var testImageFileNameStream = highland(fs.readdirSync(protocolTestResultsDirectory))
   .filter(function(fileName) {
     return fileName.indexOf('png') > -1 && fileName.indexOf('test') > -1;
-  })
-  .doto(function(testImageFileName) {
-    var imagePath = protocolTestResultsDirectory + testImageFileName;
+  });
+
+  var testImageTagStream = testImageFileNameStream.fork();
+  var imageHashStream = testImageFileNameStream.fork();
+  
+  // TODO figure out how to do this properly without redefining the variable
+  var imageHashStream2 = imageHashStream.map(function(testImageFileName) {
+    var imageSourcePath = protocolTestResultsDirectory + testImageFileName;
     var sha1Sum = crypto.createHash('sha1');
-    imageHashStream = highland(fs.ReadStream(imagePath))
-    .map(function(d) {
+    var imageBufferStream = highland(fs.ReadStream(imageSourcePath));
+
+    if (testImageFileName.indexOf('phantomjs') > -1) {
+      var imageDestinationPath = protocolTestLastKnownGoodsDirectory + testImageFileName.replace('phantomjs-test', 'lkg');
+      var dest = fs.createWriteStream(imageDestinationPath)
+      imageBufferStream.fork().pipe(dest);
+    }
+
+    return imageBufferStream.fork().map(function(d) {
       sha1Sum.update(d);
       return;
     })
@@ -29,8 +43,12 @@ gulp.task('setLastKnownGoods', function () {
       imageHashStream.resume();
       return sha1Sum.digest('hex');
     });
-    return;
-  }).map(function(testImageFileName) {
+  })
+  .sequence();
+
+  var imageHashesDestination = fs.createWriteStream(protocolTestLastKnownGoodsDirectory + 'image-hashes.json')
+
+  testImageTagStream.map(function(testImageFileName) {
     var testImageFileNameComponents = testImageFileName.split('-');
     var testImageFileNameComponentsLength = testImageFileNameComponents.length;
 
@@ -43,10 +61,13 @@ gulp.task('setLastKnownGoods', function () {
       type: type,
       name: name
     };
+    testImageTagStream.resume();
     return result;
   })
-  .zip(imageHashStream)
+  .zip(imageHashStream2)
   .reduce({}, function(imageHashes, input) {
+    // NOTE: we are setting a hash for every combination of these variables that we test:
+    // test protocol pathway name, operating system + version, browser
     var testImageTags = input[0];
     var browser = testImageTags.browser;
     var type = testImageTags.type;
@@ -55,13 +76,14 @@ gulp.task('setLastKnownGoods', function () {
     var imageHash = input[1];
 
     imageHashes[name] = imageHashes[name] || {};
-    imageHashes[name][browser] = imageHash;
+    var operatingSystem = os.type() + os.release();
+    imageHashes[name][operatingSystem] = imageHashes[name][operatingSystem] || {};
+    imageHashes[name][operatingSystem][browser] = imageHash;
     return imageHashes;
   })
-  .each(function(result) {
-    console.log(result.curves.safari);
-    console.log('result');
-    console.log(result);
-  });
+  .map(function(imageHashes) {
+    return JSON.stringify(imageHashes, null, '\t')
+  })
+  .pipe(imageHashesDestination);
 });
 
