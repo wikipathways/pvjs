bb53aa05e0a4d4d6547f4a051e9d860543e565e6
Anders Riutta <git@andersriutta.com>
2015-01-16 17:36:54 -0800

    Updated build and deploy code

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,6 +1,10 @@
+// Not sure what this task is doing or whether it's even needed.
 var gulp = require('gulp');
 
-gulp.task('copy', function() {
+gulp.task('copy', function(done) {
+  return done();
+  /*
 	return gulp.src('./lib/cross-platform-text/dist/lib/**')
 		.pipe(gulp.dest('./dist'));
+  //*/
 });
