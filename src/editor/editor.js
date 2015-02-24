bc625f308f8c340d61f8e373d375122f2e48a45c
Anders Riutta <git@andersriutta.com>
2015-02-24 14:57:59 -0800

    Fixed bugs so that now user can: 1) open annotations panel after editing, 2) see editor more quickly after clicking quick edit, 3) view selected element as highlighted in editor

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -1,6 +1,7 @@
 var _ = require('lodash');
 var insertCss = require('insert-css');
 var EditorTabsComponent = require('./editor-tabs-component/editor-tabs-component');
+var m = require('mithril');
 
 var css = [
   './editor.css'
@@ -12,13 +13,60 @@ module.exports = function(pvjs) {
   var containerElement = pvjs.$element[0][0];
   var diagramContainerElement = containerElement.querySelector('.diagram-container');
   var editorTriggerButton = containerElement.querySelector('.edit-trigger');
+  var selectedPvjsElement;
 
-  // TODO get rid of jQuery
-  $(editorTriggerButton).on('click', function(event) {
+  editorTriggerButton.addEventListener('click', function(event) {
     open();
-  });
+  }, false);
+
+  /***********************************************
+   * DataNode onclick event handler
+   **********************************************/
+  var diagramContainer = document.querySelector('.diagram-container');
+  function onClickDiagramContainer(event) {
+    m.startComputation();
+
+    var selectedElementId = event.target.id;
+
+    pvjs.publicInstance.highlighter.highlight('#' + selectedElementId);
+
+    selectedPvjsElement = pvjs.sourceData.pvjson.elements.filter(function(pvjsElement) {
+      return pvjsElement.id === selectedElementId;
+    })
+    .map(function(pvjsElement) {
+      return pvjsElement;
+    })[0];
+
+    if (!selectedPvjsElement) {
+      m.endComputation();
+      return;
+    }
+
+    editorTabsComponent.onClickDiagramContainer(selectedPvjsElement);
+
+    m.endComputation();
+  }
+
+  function clearSelection() {
+    if (selectedPvjsElement) {
+      pvjs.publicInstance.highlighter.attenuate('#' + selectedPvjsElement.id);
+    }
+
+    selectedPvjsElement = null;
+  }
+
+  function cancel() {
+    clearSelection();
+    close();
+  }
 
   function open() {
+    diagramContainer.addEventListener('click', onClickDiagramContainer, false);
+
+    // TODO this is a kludge. refactor how we avoid displaying annotation panel in edit mode.
+    document.querySelector('.annotation').style.display = 'none';
+    document.querySelector('.annotation').style.visibility = 'hidden';
+
     diagramContainerElement.setAttribute(
         'style', 'height: ' + (pvjs.elementHeight - 120) + 'px;');
     pvjs.panZoom.resizeDiagram();
@@ -27,6 +75,16 @@ module.exports = function(pvjs) {
   }
 
   function close() {
+    diagramContainer.removeEventListener('click', onClickDiagramContainer);
+
+    if (selectedPvjsElement) {
+      pvjs.publicInstance.highlighter.attenuate('#' + selectedPvjsElement.id);
+    }
+
+    // TODO this is a kludge. refactor how we avoid displaying annotation panel in edit mode.
+    document.querySelector('.annotation').style.display = null;
+    document.querySelector('.annotation').style.visibility = 'hidden';
+
     diagramContainerElement.setAttribute(
         'style', 'height: ' + pvjs.elementHeight + 'px;');
     pvjs.panZoom.resizeDiagram();
@@ -37,7 +95,9 @@ module.exports = function(pvjs) {
 
   return {
     open: open,
-    close: close
+    close: close,
+    cancel: cancel,
+    clearSelection: clearSelection
   };
 
 };
