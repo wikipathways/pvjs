6feccf8fe9d0248d5934d6e351c5cc3bb2be0a7b
Anders Riutta <git@andersriutta.com>
2015-05-04 15:26:04 -0700

    Match bdb

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,18 +1,29 @@
 var gulp = require('gulp');
 var gulpSequence = require('gulp-sequence');
 
-/* Before running this task, manually commit in master
- * Build (this task)
- *   Check git status for no changes
- *   Bump metadata files version
- *   Sync README, test and demo files with current version
- *   Build docs / Browserify
- *   Commit again with message "Built and bumped to version X.Y.Z."
- *   Create tag with new version
- * After this task, run gulp publish
+/* Task Steps
+ * Manually commit in master, then this task will:
+ * Build
+ *  - Check git status for no changes
+ *  - Bump metadata files version
+ *  - Sync README with current version
+ *  - Build docs / Browserify
+ *  - Commit again with message "Bumped to version X.Y.Z. Built"
+ *  - Create tag with new version
+ * Publish
  */
 
+gulp.task('build', gulpSequence('verify-git-status',
+      'bump-metadata-files',
+      //['browserify', 'build-docs'],
+      ['browserify'],
+      'copy'
+      //'sync-git-version'
+      ));
+
+/*
 gulp.task('build', gulpSequence(
     'browserify',
     'copy'
 ));
+//*/
