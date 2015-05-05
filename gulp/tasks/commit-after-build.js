7841e97389707196b7b928cab636c973f65b172c
Anders Riutta <git@andersriutta.com>
2015-05-05 16:18:23 -0700

    Further progress on build process.

---
+++ gulp/tasks/commit-after-build.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var git = require('gulp-git');
+var gitStreaming = require('../util/git-streaming.js');
+var gulp = require('gulp');
+var highland = require('highland');
+var metadataFilePaths = require('../util/metadata-file-paths.json');
+
+gulp.task('commit-after-build', function commitAfterBuild(callback) {
+  var package = JSON.parse(fs.readFileSync('package.json'));
+  var version = package.version;
+
+  gulp.src(['./dist/',
+            './docs/',
+            './demo/',
+            'README.md']
+            .concat(metadataFilePaths)
+  )
+  .pipe(highland.pipeline())
+  .through(git.add())
+  .through(git.commit('Built and bumped version to ' + version + '.'))
+  .last()
+  .errors(function(err, push) {
+    throw err;
+  })
+  .each(function() {
+    return callback();
+  });
+});
