c67f371d9d6532979fbe1395faef951e54246a3b
Anders Riutta <git@andersriutta.com>
2015-05-07 16:50:26 -0700

    Use test directory for pvjs-dev.bundle.js and current version for demo.

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
+var highland = require('highland');
 
 gulp.task('copy', function(done) {
-  //return done();
-  //*
   return gulp.src('./dist/**')
+    //.pipe(highland.pipeline())
 		.pipe(gulp.dest('./demo/lib/pvjs'));
-  //*/
+		.pipe(gulp.dest('./test/lib/pvjs'));
+    /*
+    .concat(
+      gulp.src('./dist/**')
+        .pipe(highland.pipeline())
+        .pipe(gulp.dest('./demo/lib/pvjs'));
+    )
+    //*/
 });
