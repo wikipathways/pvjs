4b61fd5bb8e3e0fc74e365193c5fa3f78a2680b3
Anders Riutta <git@andersriutta.com>
2015-05-07 17:06:35 -0700

    Now we can test in the test dir and the demo dir will still use the latest version.

--- gulp/tasks/copy-test-to-demo.js
+++ gulp/tasks/copy-test-to-demo.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
 var gulp = require('gulp');
 var highland = require('highland');
-var packageJson = require('../../package.json');
 var replace = require('gulp-regex-replace');
 
 gulp.task('copy-test-to-demo', function(done) {
+  // Reading it here (instead of using require) because
+  // the version may have been bumped since require ran.
+  var packageJson = JSON.parse(fs.readFileSync(
+      '../../package.json', 'utf8'));
+
   return gulp.src(['./test/lib/pvjs/**',
                    './test/web-component-demos/**',
                    './test/jquery-demos/**'],
