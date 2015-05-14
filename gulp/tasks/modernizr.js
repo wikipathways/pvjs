200df7cbacc74a2222be89ca0fcef88bf21bbfde
Anders Riutta <git@andersriutta.com>
2015-05-14 15:34:23 -0700

    Update modernizr custom build.

---
+++ gulp/tasks/modernizr.js
@@ -0,0 +1,13 @@
+var config = require('../config.json');
+var fs = require('fs');
+var gulp = require('gulp');
+var handleErrors = require('../util/handle-errors.js');
+var highland = require('highland');
+var mkdirp = require('mkdirp');
+var modernizr = require('gulp-modernizr');
+
+gulp.task('modernizr', function() {
+  gulp.src('./test/lib/pvjs/pvjs-dev.bundle.js')
+    .pipe(modernizr())
+    .pipe(gulp.dest('tmp/'));
+});
