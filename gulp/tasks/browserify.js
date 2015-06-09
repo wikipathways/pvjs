21aceddad0fe1df0b3f379882b79acd5a40b67ce
Anders Riutta <git@andersriutta.com>
2015-06-08 18:14:29 -0700

    Start splitting out viewer from non-viewer code.

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -10,6 +10,8 @@ var brfs = require('gulp-brfs');
 var browserify = require('browserify');
 var buffer = require('vinyl-buffer');
 var bundleLogger = require('../util/bundle-logger.js');
+var config = require('../config.json');
+var concat = require('concat-stream');
 var fs = require('fs');
 var gulp = require('gulp');
 var handleErrors = require('../util/handle-errors.js');
@@ -23,24 +25,54 @@ var watchify = require('watchify');
 
 gulp.task('browserify', function() {
 
-  var packageJson;
+  var packageJson = JSON.parse(fs.readFileSync('package.json'));
+  var version = packageJson.version;
+  var name = packageJson.name;
 
-  var getBundleName = function() {
-    packageJson = JSON.parse(fs.readFileSync('package.json'));
-    var version = packageJson.version;
-    var name = packageJson.name;
-    return name + '-dev.bundle';
+  mkdirp.sync('./dist/' + version + '/');
+  mkdirp.sync('./demo/lib/' + name + '/' + version + '/');
+  mkdirp.sync('./test/lib/' + name + '/' + version + '/');
+  mkdirp.sync('./test/lib/' + name + '/dev/');
+
+  var process = function(subsection) {
+    return highland.pipeline(function(stream) {
+
+      var vinylifiedStream = stream
+        // Use vinyl-source-stream to make the
+        // stream gulp compatible. Specify the
+        // desired output filename here.
+        .through(source(name + '.' + subsection + '.js'));
+
+      if (global.isWatching) {
+        vinylifiedStream
+        .pipe(gulp.dest('./test/lib/' + name + '/dev/'));
+      }
+
+      return vinylifiedStream
+        // These steps are only enabled when
+        // a watch is not set.
+        // They are too slow to enable
+        // during development.
+        .through(buffer())
+        .through(rename(function(path) {
+          path.basename = path.basename.replace(
+              '.js', '.min.js');
+        }))
+        .through(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .through(uglify())
+        // locate sourcemaps in same dir as source file
+        .through(sourcemaps.write('./'))
+        .through(gulp.dest('./test/lib/' + name + '/' + version + '/'))
+        .through(gulp.dest('./dist/' + version + '/'))
+        .pipe(gulp.dest('./demo/lib/' + name + '/' + version + '/'));
+    });
   };
 
-  var bundler = browserify({
+  var bundler = browserify(config.entries, {
     // Required watchify args
     cache: {}, packageCache: {}, fullPaths: true,
     // Browserify Options
-    // specify entry point of app
-    entries: [//'./index.js',
-      './lib/jquery-plugin.js',
-      './lib/diff-viewer/diff-viewer.js'
-    ],
     // Enable source maps!
     debug: true,
     //insertGlobals : true,
@@ -52,6 +84,13 @@ gulp.task('browserify', function() {
   .transform('brfs')
   .transform('deglobalify');
 
+  bundler.plugin('factor-bundle', {
+    outputs: config.entries.map(function(entry) {
+      return entry.split('/').pop().replace('.js', '');
+    })
+    .map(process)
+  });
+
   var bundle = function() {
     // Log when bundling starts
     bundleLogger.start();
@@ -60,34 +99,7 @@ gulp.task('browserify', function() {
     .bundle()
     // Report compile errors
     .on('error', handleErrors)
-    // Use vinyl-source-stream to make the
-    // stream gulp compatible. Specify the
-    // desired output filename here.
-    .pipe(source(getBundleName() + '.js'))
-    .pipe(highland.pipeline(function(stream) {
-      if (global.isWatching) {
-        return stream;
-      }
-
-      return stream
-        // These steps are only enabled when
-        // a watch is not set.
-        // They are too slow to enable
-        // during development.
-        .through(buffer())
-        .through(rename(function(path) {
-          path.basename = path.basename.replace(
-              '-dev.bundle', '-' + packageJson.version + '.bundle.min');
-        }))
-        .through(sourcemaps.init({loadMaps: true}))
-        // Add transformation tasks to the pipeline here.
-        .through(uglify())
-        .through(sourcemaps.write('./'))
-        .through(gulp.dest('./dist/'))
-        .through(gulp.dest('./demo/lib/' + packageJson.name + '/'));
-    }))
-    // Specify the output destination
-    .pipe(gulp.dest('./test/lib/' + packageJson.name + '/'))
+    .pipe(process('core'))
     // Log when bundling completes!
     .on('end', bundleLogger.end);
   };
