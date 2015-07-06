041ce3e9252f006d5d36067d30b196dd6f8c22d1
Anders Riutta <git@andersriutta.com>
2015-07-06 16:22:04 -0700

    Bumped and built. Close #306.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,5 +1,12 @@
+var colors = require('colors');
 var gulp = require('gulp');
 var path = require('path');
+
+var changeTypeToColorMappings = {
+  'added': 'green',
+  'changed': 'yellow',
+  'deleted': 'red'
+};
 
 gulp.task('watch', ['set-watch', 'browser-sync'], function() {
 	// Note: The browserify task handles js recompiling with watchify
@@ -15,10 +22,13 @@ gulp.task('watch', ['set-watch', 'browser-sync'], function() {
   .forEach(function(dirName) {
     var packageName = dirName.replace(/-js$/, '').replace(/\.js$/, '').replace(/js$/, '');
     var remoteBase = path.resolve(path.join('..', dirName, 'lib'));
-    var watchTarget = path.join(remoteBase, '**/*.js');
-    gulp.watch(watchTarget, function(event) {
-      console.log(packageName + ' file ' + event.path + ' was ' + event.type + '.');
+    var watchTargetJs = path.join(remoteBase, '**/*.js');
+    var watchTargetCss = path.join(remoteBase, '**/*.css');
+    var watchTargetHtml = path.join(remoteBase, '**/*.html');
+    gulp.watch([watchTargetJs, watchTargetCss, watchTargetHtml], function(event) {
       var type = event.type;
+      console.log(type[changeTypeToColorMappings[type]] + ': ' +
+          event.path.replace(packageName, packageName.inverse));
       if (['changed', 'added'].indexOf(type) > -1) {
         var srcPath = event.path;
         var localBase = path.resolve(path.join('.', 'node_modules', packageName, 'lib'));
