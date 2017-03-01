3aac06e7b4d08df57aa2882e3e909bb566173502
Jacob Windsor <jacob.windsor@ymail.com>
2017-03-01 13:57:20 +0100

    Add docblock to wrapper

--- src/Wrapper/Wrapper.tsx
+++ src/Wrapper/Wrapper.tsx
@@ -3,6 +3,13 @@ import * as $ from 'jquery';
 import * as ReactDOM from 'react-dom';
 import {Pvjs} from '../PVJS/PVJS'
 
+/**
+ * Simple wrapper around the PVJS react component.
+ * Use this function to call PVJS
+ * @param selector: DOM Selector
+ * @param WPId: WikiPathways ID to get the desired pathway
+ * @param customStyle
+ */
 export function pvjs(selector, WPId, customStyle){
     let container = $(selector).get(0);
     ReactDOM.render(
