189ec302e4a3e8491740f5560d693ba61430f742
Jacob Windsor <jacob.windsor@ymail.com>
2017-03-07 13:31:34 +0100

    refactor manipulator

--- lib/manipulation-api/manipulator.ts
+++ lib/manipulation-api/manipulator.ts
@@ -1,15 +1,18 @@
 import * as _ from 'lodash';
 import $ = require('jquery');
 import d3 = require('d3');
+import * as SvgPanZoom from 'svg-pan-zoom'
 
 export class Manipulator {
     private pvjs_instance: Pvjs;
     private highlightedNodes: Array<string>;
+    private panZoom;
 
     constructor(pvjs_instance: Pvjs){
-        console.log("Successfully loaded the manipulation API!");
         this.pvjs_instance = pvjs_instance;
         this.highlightedNodes = [];
+
+        this.panZoom = pvjs_instance.panZoom // TODO: This is a hack for now. Fix this
     }
 
     /**
@@ -25,7 +28,7 @@ export class Manipulator {
      * TODO: Either write a new highlighter library or rewrite the kaavio one
      *
      */
-    toggleHighlight(node_id: any, colour, resetOthers: boolean = true): void {
+    toggleHighlight(node_id: string, colour: string, resetOthers: boolean = true): void {
         if (typeof node_id === 'string'){
             // Just one node_id
             let arr = [];
@@ -47,7 +50,7 @@ export class Manipulator {
      * @param node_id
      * @param colour
      */
-    private toggleHighlightOfOneNode(node_id: string, colour){
+    private toggleHighlightOfOneNode(node_id: string, colour: string): void {
         let id = '#' + node_id;
         if(this.isHighlighted(node_id)) {
             this.highlightOff(node_id);
@@ -68,7 +71,7 @@ export class Manipulator {
      * Push the node_id onto the highlightedNodes array
      * @param node_id
      */
-    private addHighlighted(node_id): void {
+    private addHighlighted(node_id: string): void {
         this.highlightedNodes.push(node_id);
     }
 
@@ -76,7 +79,7 @@ export class Manipulator {
      * Delete the node_id from the highlightedNodes array
      * @param node_id
      */
-    private delHighlighted(node_id): void {
+    private delHighlighted(node_id: string): void {
         this.delFromArray(this.highlightedNodes, node_id);
     }
 
@@ -98,7 +101,7 @@ export class Manipulator {
      * @param node_id
      * @returns {boolean}
      */
-    private isHighlighted(node_id): boolean {
+    private isHighlighted(node_id: string): boolean {
         let index = this.highlightedNodes.indexOf(node_id);
         return index !== -1;
     }
@@ -107,7 +110,7 @@ export class Manipulator {
      * Un-highlight a node.
      * @param node_id
      */
-    private highlightOff(node_id): void {
+    private highlightOff(node_id: string): void {
         let id = '#' + node_id;
         this.pvjs_instance.highlighter.attenuate(id);
         this.delHighlighted(node_id);
@@ -129,23 +132,21 @@ export class Manipulator {
         });
     }
 
-    private findNode(node_id): any {
+    /**
+     * Return the node element reference by the node_id
+     * @param node_id
+     * @returns {SVGLocatable}
+     */
+    private findNode(node_id): SVGLocatable {
         return d3.select(this.pvjs_instance.selector).select("g path#" + node_id)[0][0];
     }
 
-    // private convertCoords(x,y) {
-    //
-    // var offset = svgDoc.getBoundingClientRect();
-    //
-    // var matrix = elem.getScreenCTM();
-    //
-    //     return {
-    //         x: (matrix.a * x) + (matrix.c * y) + matrix.e - offset.left,
-    //         y: (matrix.b * x) + (matrix.d * y) + matrix.f - offset.top
-    //     };
-    // }
-
-    private getNodeCoordinates(node_id): any {
+    /**
+     * Return the coordinates of the center of the node referenced by the node_id
+     * @param node_id
+     * @returns {{x: number, y: number}}
+     */
+    private getNodeCoordinates(node_id: string): {x: number, y: number} {
         let node = this.findNode(node_id);
         let clientRect = node.getBBox();
 
@@ -157,11 +158,9 @@ export class Manipulator {
         let centreX = x + (width/2);
         let centreY = y + (height/2);
 
-        const containerHeight = this.pvjs_instance.panZoom.getSizes().height;
-        const containerWidth = this.pvjs_instance.panZoom.getSizes().width;
-        console.log(containerHeight);
-        console.log(containerWidth);
-        let realZoom = this.pvjs_instance.panZoom.getSizes().realZoom;
+        const containerHeight = this.panZoom.getSizes().height;
+        const containerWidth = this.panZoom.getSizes().width;
+        let realZoom = this.panZoom.getSizes().realZoom;
         const offsetTop = containerHeight / 2;
         const offsetLeft = containerWidth / 2;
 
@@ -171,14 +170,39 @@ export class Manipulator {
         }
     }
 
-    private computeZoom(node_id): any {
+    /**
+     * Return the coordinates that should be panned to to get the desired node into the center of the diagram
+     * @param node_id
+     * @returns {{x: number, y: number}}
+     */
+    private getNodeCoordinatesWithOffset(node_id: string): {x: number, y:number}{
+        const coordinates = this.getNodeCoordinates(node_id);
+
+        const containerHeight = this.panZoom.getSizes().height;
+        const containerWidth = this.panZoom.getSizes().width;
+        let realZoom = this.panZoom.getSizes().realZoom;
+        const offsetTop = containerHeight / 2;
+        const offsetLeft = containerWidth / 2;
+
+        coordinates.x += offsetLeft;
+        coordinates.y += offsetTop;
+
+        return coordinates;
+    }
+
+    /**
+     * Compute the amount that the node should be zoomed in by
+     * @param node_id
+     * @returns {number}
+     */
+    private computeZoom(node_id: string): number {
         let node = this.findNode(node_id);
         const BBox = node.getBBox();
 
         const nodeArea = BBox.width * BBox.height;
         
 
-        const containerSize = this.pvjs_instance.panZoom.getSizes();
+        const containerSize = this.panZoom.getSizes();
         const containerArea = containerSize.width * containerSize.height;
 
         const relativeArea = containerArea /nodeArea;
@@ -186,33 +210,53 @@ export class Manipulator {
         return relativeArea * scalingFactor;
     }
 
-    zoom(zoom_perc): any {
-        this.pvjs_instance.panZoom.zoom(zoom_perc);
-        //this.pvjs_instance.panZoom.pan(origin);
+    /**
+     * Zoom in
+     * @param zoom_perc
+     */
+    zoom(zoom_perc: number): void {
+        this.panZoom.zoom(zoom_perc);
     }
 
-    zoomOn(node_id): any {
+    /**
+     * Zoom onto a specific node
+     * @param node_id
+     */
+    zoomOn(node_id: string): void {
         let zoom_perc = this.computeZoom(node_id);
         this.zoom(zoom_perc);
         this.panTo(node_id);
     }
 
-    resetZoom(): any {
-        this.pvjs_instance.panZoom.reset();
+    /**
+     * Reset the zoom
+     */
+    resetZoom(): void {
+        this.panZoom.resetZoom();
     }
 
-    pan(coordinates): any {
-        this.pvjs_instance.panZoom.pan(coordinates);
-
+    /**
+     * Pan to a specific set of coordinates
+     * @param coordinates
+     */
+    pan(coordinates: {x: number, y: number}): void {
+        this.panZoom.pan(coordinates);
     }
 
-    panTo(node_id): any {
+    /**
+     * Pan to a specific node
+     * @param node_id
+     */
+    panTo(node_id: string): void {
         let coordinates = this.getNodeCoordinates(node_id);
         this.pan(coordinates);
     }
 
-    resetPan(): any {
-
+    /**
+     * Reset the pan
+     */
+    resetPan(): void {
+        this.panZoom.resetPan();
     }
 
     toggleAnnotations(node_id): any {
