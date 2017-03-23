cac6f6b929f3d6838c4cf67ba9b859929a8c119a
Jacob Windsor <jacob.windsor@ymail.com>
2017-03-23 16:55:20 +0100

    Add docblocks

--- src/Kaavio/manipulator/manipulator.ts
+++ src/Kaavio/manipulator/manipulator.ts
@@ -3,6 +3,11 @@ import $ = require('jquery');
 import d3 = require('d3');
 import * as ReactDOM from 'react-dom';
 
+/**
+ * Class for "The Manipulation API".
+ * Really, this is just a wrapper around public functions within Kaavio components with a few extras.
+ * Implements highlighting and zooming/panning to nodes or groups of nodes.
+ */
 export class Manipulator {
     private diagram;
     private panZoom;
@@ -10,7 +15,6 @@ export class Manipulator {
     private relPoint: any; // A point used to calculate element positions
 
     constructor(kaavioRef, panZoomRef){
-
         // Subscribe to the panZoomEnabled observable, wait for panZoom to be ready.
         kaavioRef.kaavioReady$.subscribe(res => {
             if(res === true) {
@@ -58,6 +62,14 @@ export class Manipulator {
         this.highlightOnOne(node_id, color);
     }
 
+    /**
+     * Turn on the highlighting of one entity.
+     * Behaviour is to only change the highlighted entities if the node_id or color changes.
+     * @param node_id - one identifier or a string of identifiers
+     * @param color - can be any css colour
+     * @param resetOthers - Reset all other highlighted nodes before highlighting. Default = true
+     * @param resetPanZoom - reset the pan & zoom before highlighting. Default = true
+     */
     highlightOn(node_id: any, color: string, resetOthers: boolean = true, resetPanZoom: boolean = true): void {
         if(resetPanZoom) this.resetPanZoom();
         if(! color) throw new Error("No color specified.");
@@ -78,6 +90,12 @@ export class Manipulator {
         });
     }
 
+    /**
+     * Turn off the highlighting of one or multiple nodes
+     * @param node_id - one identifier or a string of identifiers
+     * @param resetOthers - Reset all other highlighted nodes before highlighting. Default = true
+     * @param resetPanZoom - reset the pan & zoom before highlighting. Default = true
+     */
     highlightOff(node_id: any, resetOthers: boolean = true, resetPanZoom: boolean = true): void {
         if(resetPanZoom) this.resetPanZoom();
 
@@ -135,6 +153,11 @@ export class Manipulator {
         return d3.select(this.diagram).select(".svg-pan-zoom_viewport")[0][0]
     }
 
+    /**
+     * Get the bounding box of an entity.
+     * @param node_id
+     * @returns {{x: any, y: any, height: number, width: number}}
+     */
     private getNodeBBox(node_id: string): {x: number, y: number, height: number, width: number} {
         let node = this.findNode(node_id);
         let viewport = this.getViewport();
@@ -155,7 +178,7 @@ export class Manipulator {
     }
 
     /**
-     * Get the bounding box for a group of nodes.
+     * Get the bounding box for a group of entities.
      * Note: if there is a huge number of node_ids this might take a while...
      * @param node_ids
      * @returns {{x: null, y: null, height: number, width: number}}
