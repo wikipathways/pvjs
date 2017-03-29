3e8087cfd6f75a1f589fd41e5c3acc4b6aed113d
Jacob Windsor <jacob.windsor@ymail.com>
2017-03-29 11:41:45 +0200

    Tidy up Pvjs

--- src/Pvjs.tsx
+++ src/Pvjs.tsx
@@ -72,25 +72,159 @@ export class Pvjs extends React.Component<any, any> {
 	kaavioRef: any;
 	manipulator: Manipulator;
 
-  constructor(props) {
-		const customStyle = props.customStyle || {};
+  	constructor(props) {
 		super(props);
 		this.state = {
-			about: props.about,
-			version: props.version,
-			src: props.src,
-			alt: props.alt,
-			customStyle: props.customStyle,
-			pvjson: {
-				entities: [],
-				organism: '',
-				name: '',
-			},
-			filters: [],
-			selected: null,
+			pvjson: null,
+			filters: null,
+			loading: false,
+			loaded: false,
 			detailPanelOpen: false,
+			selected: null,
+			error: null
 		};
-  }
+  	}
+
+	getPathway() {
+  		this.setState({loading: true});
+		const { about, version } = this.props;
+		// TODO handle version
+		const src = about.replace(/.*wikipathways[:\/]/, 'http://webservice.wikipathways.org/getPathwayAs?fileType=xml&format=json&pwId=');
+
+		const ajaxRequest: AjaxRequest = {
+			url: src,
+			method: 'GET',
+			responseType: 'json',
+			timeout: 1000, // ms
+			crossDomain: true,
+		};
+
+		const observable = Observable.ajax(ajaxRequest)
+			.map((ajaxResponse): {data: string} => ajaxResponse.xhr.response)
+            .map(res => Base64.decode(res.data));
+
+		return gpml2pvjson(observable, about).subscribe((pvjson) => {
+			const { entities, organism, name } = pvjson;
+
+			pvjson.entities = entities.map((entity) => {
+				if (entity.displayName) {
+					// Set the inner text to the displayName
+					// We don't need displayName
+					entity.textContent = entity.displayName;
+				}
+				// Remove the displayName
+				return omit(entity, ['displayName']);
+			});
+
+			// Build up the title for the diagram
+			const infoBoxTextContent = `Title: ${name} \n Organism: ${organism}`;
+			// Add on the info box containing the title
+			pvjson.entities = entities.concat([{
+				id: 'pvjs-infobox',
+				kaavioType: 'Node',
+				drawAs: 'None',
+				backgroundColor: 'transparent',
+				borderWidth: 0,
+				color: '#999999',
+				fontSize: 14,
+				textContent: infoBoxTextContent,
+				textAlign: 'start',
+				type: [
+					'Node',
+					'Label',
+					'InfoBox',
+				],
+				padding: '0.1em',
+				align: 'left',
+				verticalAlign: 'middle',
+				fontFamily: 'Arial',
+				x: 5,
+				y: 5,
+				height: 50,
+				width: infoBoxTextContent.length * 3.5,
+				zIndex: entities.length + 1,
+			}]);
+
+			// If an entity has citations, format the citation and add it to the entities array
+			pvjson.entities = pvjson.entities.reduce((acc, entity) => {
+				if (entity.hasOwnProperty('citation')) {
+					const burrs = entity.burrs || [];
+					const {citation, kaavioType} = entity;
+
+					const citationBurrId = `citation-burr-${citation.join('-')}-for-${entity.id}`;
+					burrs.push(citationBurrId);
+					entity.burrs = burrs;
+
+					const citationDisplayString = createPublicationXrefString(
+						citation
+							.map(cId => find(pvjson.entities, {id: cId}))
+							.map(c => c.textContent)
+					);
+					const citationBurr = {
+						id: citationBurrId,
+						type: ['Citation', 'Burr'],
+						width: citationDisplayString.length * 1.5,
+						height: 12,
+						textContent: citationDisplayString,
+						drawAs: 'None',
+						attachmentDisplay: {
+							position: kaavioType === 'Edge' ? [0.5] : [1, 0],
+							offset: kaavioType === 'Edge' ? [5, 5] : [0, -5],
+						},
+					};
+					acc.push(citationBurr);
+				}
+				acc.push(entity);
+				return acc;
+			}, []);
+
+			// Add filters if a double line style is specified in the entity
+			// Anders: We discussed just doing this with CSS. What's the verdict?
+			const filters = pvjson.entities
+				.filter((entity) => entity.lineStyle === 'double')
+				.reduce((acc, entity) => {
+					const lineStyle = entity.lineStyle;
+					let entityFilters = [];
+					if (entity.filter) {
+						entityFilters.push(entity.filter);
+					}
+					const strokeWidth = entity.borderWidth;
+					entityFilters.push(generateFilterId('doubleStroke', strokeWidth));
+					const filterId = entityFilters.join('_');
+					// NOTE: notice side effect
+					entity.filter = filterId;
+					acc.push(filterId);
+					return acc;
+				}, []);
+
+			// Reduce the filters further. Add any children needed.
+			filters.reduce((acc: any, filterId: string) => {
+				const filterChildren = filterId.split('_').reduce((subAcc, subFilter: string) => {
+					const [filterName, strokeWidthAsString] = subFilter.split('-');
+					const strokeWidth = parseFloat(strokeWidthAsString);
+
+					if(filterName !== 'doubleStroke') return;
+
+					doubleStroke({
+						source: filterId.indexOf('ound') > -1 ? 'roundResult' : 'SourceGraphic',
+						strokeWidth: strokeWidth,
+					}).forEach(x => {
+						subAcc.push(x);
+					});
+
+					return subAcc;
+				}, []);
+				acc.push(<Filter id={filterId} key={filterId} children={filterChildren} />);
+				return acc;
+			}, []);
+
+			this.setState({pvjson: pvjson, filters: filters, loaded: true, loading: false});
+		}, (err) => {
+			err.message = err.message || '';
+			err.message += ' Error getting pathway (is webservice.wikipathways.org down?)';
+			this.setState({error: err, loaded: false, loading: false})
+		});
+	}
 
 	closeActive() {
 		this.setState({selected: null, detailPanelOpen: false})
@@ -104,191 +238,66 @@ export class Pvjs extends React.Component<any, any> {
 		}
 	}
 
-	getPathway() {
-		let that = this;
-		const state = that.state;
-		const { about, version } = state;
-		// TODO handle version
-		const src = state.src ||
-			about.replace(/.*wikipathways[:\/]/, 'http://webservice.wikipathways.org/getPathwayAs?fileType=xml&format=json&pwId=');
-		const ajaxRequest: AjaxRequest = {
-			url: src,
-			method: 'GET',
-			responseType: 'json',
-			timeout: 1 * 1000, // ms
-			crossDomain: true,
-		};
-		return gpml2pvjson(
-				Observable.ajax(ajaxRequest)
-					.map((ajaxResponse): {data: string} => ajaxResponse.xhr.response)
-					.map(res => Base64.decode(res.data)),
-				about
-		)
-			.subscribe(function(pvjson) {
-				let { entities, organism, name } = pvjson;
-
-				const infoBoxTextContent = `Title: ${name} \n Organism: ${organism}`;
-
-				pvjson.entities = entities.map(function(entity) {
-					const displayName = entity.displayName;
-					if (displayName) {
-						entity.textContent = displayName;
-					}
-					return omit(entity, ['displayName']);
-				}).concat([{
-					id: 'pvjs-infobox',
-					kaavioType: 'Node',
-					drawAs: 'None',
-					backgroundColor: 'transparent',
-					borderWidth: 0,
-					color: '#999999',
-					fontSize: 14,
-					textContent: infoBoxTextContent,
-					textAlign: 'start',
-					type: [
-						'Node',
-						'Label',
-						'InfoBox',
-					],
-					padding: '0.1em',
-					align: 'left',
-					verticalAlign: 'middle',
-					fontFamily: 'Arial',
-					x: 5,
-					y: 5,
-					height: 50,
-					width: infoBoxTextContent.length * 3.5,
-					zIndex: entities.length + 1,
-				}]);
-
-				pvjson.entities = pvjson.entities
-					.reduce(function(acc, entity) {
-						if (entity.hasOwnProperty('citation')) {
-							const burrs = entity.burrs || [];
-							const {citation, x, y, width, height, kaavioType, points} = entity;
-
-							const citationBurrId = `citation-burr-${citation.join('-')}-for-${entity.id}`;
-							burrs.push(citationBurrId);
-							entity.burrs = burrs;
-
-							const citationDisplayString = createPublicationXrefString(
-									citation
-										.map(cId => find(pvjson.entities, {id: cId}))
-										.map(c => c.textContent)
-							);
-							const citationBurr = {
-								id: citationBurrId,
-								type: ['Citation', 'Burr'],
-								width: citationDisplayString.length * 1.5,
-								height: 12,
-								textContent: citationDisplayString,
-								drawAs: 'None',
-								attachmentDisplay: {
-									position: kaavioType === 'Edge' ? [0.5] : [1, 0],
-									offset: kaavioType === 'Edge' ? [5, 5] : [0, -5],
-								},
-							};
-							acc.push(citationBurr);
-						}
-						acc.push(entity);
-						return acc;
-					}, []);
-
-				const filters = Array.from(
-						pvjson.entities
-							.filter((entity) => entity.lineStyle === 'double')
-							.reduce(function(acc, entity) {
-								const lineStyle = entity.lineStyle;
-								let entityFilters = [];
-								if (entity.filter) {
-									entityFilters.push(entity.filter);
-								}
-								const strokeWidth = entity.borderWidth;
-								entityFilters.push(generateFilterId('doubleStroke', strokeWidth));
-								const filterId = entityFilters.join('_');
-								// NOTE: notice side effect
-								entity.filter = filterId;
-								acc.add(filterId);
-								return acc;
-							}, new Set())
-				)
-					.reduce(function(acc: any, filterId: string) {
-						const filterChildren = filterId.split('_').reduce(function(subAcc, subFilter: string): any[] {
-							const [filterName, strokeWidthAsString] = subFilter.split('-');
-							const strokeWidth = parseFloat(strokeWidthAsString);
-
-							if (filterName === 'doubleStroke') {
-								doubleStroke({
-									source: filterId.indexOf('ound') > -1 ? 'roundResult' : 'SourceGraphic',
-									strokeWidth: strokeWidth,
-								})
-									.forEach(function(x) {
-										subAcc.push(x);
-									});
-							}
-
-							return subAcc;
-						}, []);
-						acc.push(<Filter id={filterId} key={filterId} children={filterChildren} />);
-						return acc;
-					}, []);
-
-				that.setState({pvjson: pvjson, filters: filters});
-			}, function(err) {
-				err.message = err.message || '';
-				err.message += ' Error getting pathway (is webservice.wikipathways.org down?)'
-				console.error(err);
-				that.setState({});
-			})
+	componentWillMount() {
+		this.getPathway();
 	}
 
-	componentDidMount() {
-		let that = this;
-		that.getPathway();
-	}
-
-	componentDidUpdate(){
-		this.manipulator = this.kaavioRef.manipulator;
-	}
-
-	// TODO is this correct? Or should we use componentWillUpdate?
-//	componentDidUpdate(prevProps, prevState) {
-//		let that = this;
-//		const state = that.state;
-//		if (JSON.stringify(prevState.pvjson) !== JSON.stringify(state.pvjson)) {
-//			that.getPathway();
-//		}
-//	}
+	// componentDidUpdate(){
+	// 	this.manipulator = this.kaavioRef.manipulator;
+	// }
 
 	componentWillReceiveProps(nextProps) {
-		let that = this;
-		const prevProps = that.props;
-		forOwn(nextProps, function(prop, key) {
-			if (prop && JSON.stringify(prevProps[key]) !== JSON.stringify(prop)) {
-				that.setState({
-					[key]: prop,
-				});
-			}
-		});
-	}
+		const prevProps = this.props;
+		if (nextProps.about !== prevProps.about || nextProps.version !== prevProps.version) {
+			// Reset the state
+			this.setState({
+				pvjson: null,
+				filter: null,
+				loading: false,
+				loaded: false,
+				detailPanelOpen: false,
+				selected: null,
+				error: null
+			});
+			this.getPathway();
+		}
+	};
 
 	componentWillUnmount() {
-		let that = this;
 		// TODO cancel any pending network requests, possibly something like this:
-		//that.pathwayRequest.dispose();
+		//this.pathwayRequest.dispose();
 	}
 
 	handleCloseDetailsPanel() {
 		this.setState({detailPanelOpen: false})
 	}
 
-  render() {
-		let that = this;
-		const state = that.state;
-		const { about, customStyle, detailPanelOpen, pvjson, filters, selected } = state;
+	renderDetailsPanel() {
+		const {pvjson, selected, detailPanelOpen} = this.state;
+		if(! detailPanelOpen) return null;
 
-		return <section>
-			<Kaavio ref={kaavio => this.kaavioRef = kaavio} handleClick={that.handleClick.bind(that)} about={about}
+		return (
+			<XrefsAnnotationPanel
+				bridgeDb={new BridgeDb()}
+				organism={pvjson.organism}
+				entityType={!!selected && selected.wpType}
+				displayName={!!selected && selected.textContent}
+				dataSource={selected && selected.dbName}
+				identifier={!!selected && selected.dbId}
+				handleClose={e => this.handleClick(e)}
+			/>
+		)
+	}
+
+  	render() {
+		const {loading, loaded, error, pvjson, filters} = this.state;
+		const { about, customStyle} = this.props;
+
+		if(loading) return null;
+
+		return (
+			<section>
+				<Kaavio ref={kaavio => this.kaavioRef = kaavio} handleClick={e => this.handleClick(e)} about={about}
 							entities={pvjson.entities}
 							name={pvjson.name}
 							width={pvjson.width}
@@ -296,20 +305,9 @@ export class Pvjs extends React.Component<any, any> {
 							backgroundColor={pvjson.backgroundColor}
 							customStyle={customStyle}
 							edgeDrawers={EdgeDrawers} icons={icons} markerDrawers={markerDrawers} filters={filters} />
-			{
-					detailPanelOpen ?
-							<XrefsAnnotationPanel
-									bridgeDb={new BridgeDb()}
-									organism={pvjson.organism}
-									entityType={!!selected && selected.wpType}
-									displayName={!!selected && selected.textContent}
-									dataSource={selected && selected.dbName}
-									identifier={!!selected && selected.dbId}
-									handleClose={that.handleCloseDetailsPanel.bind(that)}
-							/>: <g />
-			}
-		</section>
+
+				{this.renderDetailsPanel()}
+			</section>
+		)
 	}
 }
-
-export default Pvjs;
