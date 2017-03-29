e91b803c704a1ab7e0dd62867f72c06e6bd12a25
Jacob Windsor <jacob.windsor@ymail.com>
2017-03-29 12:24:35 +0200

    Get panZoom working with state refs

--- src/Kaavio/Kaavio.tsx
+++ src/Kaavio/Kaavio.tsx
@@ -24,17 +24,15 @@ export class Kaavio extends React.Component<any, any> {
 	private kaavioReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
 	kaavioReady$: Observable<boolean> = this.kaavioReady.asObservable();
 
-	diagramRef: any;
-	panZoomRef: any;
 	manipulator: Manipulator;
-	private highlightedNodes: highlightedNode[] = []; // A stack of the highlighted nodes that is pushed and popped
-	private hiddenEntities: string[] = [];
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			highlightedNodes: [],
-			hiddenEntities: [],
+			diagramRef: null,
+			panZoomRef: null,
+			highlightedNodes: [], // A stack of the highlighted nodes that is pushed and popped
+			hiddenEntities: [], // A stack of hidden entities that is pushed and popped
 		};
 
 		normalize();
@@ -42,24 +40,10 @@ export class Kaavio extends React.Component<any, any> {
 		setupPage('#' + this.props.about);
 	}
 
-	componentWillReceiveProps(nextProps) {
-		let that = this;
-		const prevProps = that.props;
-		forOwn(nextProps, function(prop, key) {
-			if (key === 'filters') {
-				that.setState({
-					[key]: prop,
-				});
-			} else if (prop && JSON.stringify(prevProps[key]) !== JSON.stringify(prop)) {
-				that.setState({
-					[key]: prop,
-				});
-			}
-		});
-	}
-
 	componentDidMount(){
-		this.panZoomRef.panZoomEnabled.subscribe(res => {
+		console.log("mounted");
+
+		this.state.panZoomRef.panZoomEnabled.subscribe(res => {
 			if(res) this.kaavioReady.next(true);
 		});
 		this.setupManipulator();
@@ -67,7 +51,7 @@ export class Kaavio extends React.Component<any, any> {
 
 	private setupManipulator(): void {
 		if(this.manipulator) this.manipulator = null;
-		this.manipulator = new Manipulator(this, this.panZoomRef);
+		this.manipulator = new Manipulator(this, this.state.panZoomRef);
 	}
 
 	pushHighlighted = (highlighted: highlightedNode | highlightedNode[]) =>  {
@@ -78,14 +62,16 @@ export class Kaavio extends React.Component<any, any> {
 		else {
 			toHighlight = highlighted;
 		}
+
+		const {highlightedNodes} = this.state;
+
 		// Remove any items from the current highlightedNodes array with the same node_id
-		_.pullAllWith(this.highlightedNodes, toHighlight, (arrVal, othVal) => {
+		_.pullAllWith(highlightedNodes, toHighlight, (arrVal, othVal) => {
 			return arrVal.node_id == othVal.node_id;
 		});
 
-		this.highlightedNodes = this.highlightedNodes.concat(toHighlight);
 		this.setState({
-			highlightedNodes: this.highlightedNodes
+			highlightedNodes: highlightedNodes.concat(toHighlight)
 		});
 	};
 
@@ -97,16 +83,18 @@ export class Kaavio extends React.Component<any, any> {
 		else {
 			toRemove = node_id;
 		}
+		const {highlightedNodes} = this.state;
 
 		// Remove any items from the current highlightedNodes array with the same node_id
-		_.pullAllWith(this.highlightedNodes, toRemove, (arrVal, othVal) => {
+		_.pullAllWith(highlightedNodes, toRemove, (arrVal, othVal) => {
 			return arrVal.node_id == othVal;
 		});
-		this.setState({highlightedNodes: this.highlightedNodes});
+		this.setState({highlightedNodes: highlightedNodes});
 	};
 
 	resetHighlighted = (exclude?: string[]) => {
-		let toReset = this.highlightedNodes.map(highlightedNode => {
+		const {highlightedNodes} = this.state;
+		let toReset = highlightedNodes.map(highlightedNode => {
 			return highlightedNode.node_id;
 		});
 		if(exclude){
@@ -116,7 +104,8 @@ export class Kaavio extends React.Component<any, any> {
 	};
 
 	isHighlighted = (node_id: string) => {
-		return !!this.highlightedNodes.find(elem => {
+		const {highlightedNodes} = this.state;
+		return !!highlightedNodes.find(elem => {
 			return elem.node_id === node_id;
 		});
 	};
@@ -129,14 +118,16 @@ export class Kaavio extends React.Component<any, any> {
 		else {
 			toHide = entity_id;
 		}
+
+		const {hiddenEntities} = this.state;
+
 		// Remove any items from the current highlightedNodes array with the same node_id
-		_.pullAllWith(this.hiddenEntities, toHide, (arrVal, othVal) => {
+		_.pullAllWith(hiddenEntities, toHide, (arrVal, othVal) => {
 			return arrVal == othVal;
 		});
 
-		this.hiddenEntities = this.hiddenEntities.concat(toHide);
 		this.setState({
-			hiddenEntities: this.hiddenEntities
+			hiddenEntities: hiddenEntities.concat(toHide)
 		});
 	};
 
@@ -149,15 +140,19 @@ export class Kaavio extends React.Component<any, any> {
 			toRemove = entity_id;
 		}
 
+		const {hiddenEntities} = this.state;
+
 		// Remove any items from the current highlightedNodes array with the same node_id
-		_.pullAllWith(this.hiddenEntities, toRemove, (arrVal, othVal) => {
+		_.pullAllWith(hiddenEntities, toRemove, (arrVal, othVal) => {
 			return arrVal == othVal;
 		});
-		this.setState({hiddenEntities: this.hiddenEntities});
+		this.setState({hiddenEntities: hiddenEntities});
 	};
 
 	resetHidden = (exclude?: string[]) => {
-		let toReset = this.hiddenEntities;
+		const {hiddenEntities} = this.state;
+
+		let toReset = hiddenEntities;
 		if(exclude){
 			toReset = _.pullAll(toReset, exclude);
 		}
@@ -165,16 +160,18 @@ export class Kaavio extends React.Component<any, any> {
 	};
 
 	isHidden = (entity_id: string) => {
-		return this.hiddenEntities.indexOf(entity_id) > -1;
+		const {hiddenEntities} = this.state;
+		return hiddenEntities.indexOf(entity_id) > -1;
 	};
 
 	render() {
-		const { propsAbout, customStyle, filters, handleClick, entities, name, width, height, edgeDrawers, icons,
-			markerDrawers, propsBackgroundColor = 'white' } = this.props;
+		const {customStyle, filters, handleClick, entities, name, width, height, edgeDrawers, icons,
+			markerDrawers} = this.props;
 		const {highlightedNodes , hiddenEntities} = this.state;
 
-		const backgroundColor = customStyle.backgroundColor || propsBackgroundColor;
-		const about = propsAbout || ('kaavio-container-' + new Date().toISOString()).replace(/\W/g, '');
+		const backgroundColor = customStyle.backgroundColor || 'white' ;
+		let {about} = this.props;
+		about = about || ('kaavio-container-' + new Date().toISOString()).replace(/\W/g, '');
 
 		const entityMap = entities.reduce(function(acc, entity) {
 			acc[entity.id] = entity;
@@ -193,10 +190,13 @@ export class Kaavio extends React.Component<any, any> {
 			})
 			.map((entity) => entity.id);
 
+		// TODO: Don't use refs!
+		// Accessing the diagram ref from the state is a little bit of a hack to get panZoom working.
+		// Consider refactoring the panZoom working to be truly Reactive and not use refs
 		return (
 			<div id={about} width={width} height={height} className={`kaavio-container ${ customStyle.containerClass }`}>
 				<Diagram
-					ref={diagram => this.diagramRef = diagram}
+					ref={diagram => !this.state.diagramRef && this.setState({diagramRef: diagram})}
 					about={`kaavio-diagram-for-${about}`}
 					name={name}
 					width={width}
@@ -213,7 +213,8 @@ export class Kaavio extends React.Component<any, any> {
 					highlightedNodes={highlightedNodes}
 					hiddenEntities={hiddenEntities}
 				/>
-				<PanZoom ref={panZoom => this.panZoomRef = panZoom} diagram={this.diagramRef} />
+				<PanZoom ref={panZoom => !this.state.panZoom && this.setState({panZoom: panZoom})}
+						 diagram={this.state.diagramRef} />
 			</div>
 		)
 	}
