584181dea07c63f779a147187170d1f59bdf3f94
Jacob Windsor <jacob.windsor@ymail.com>
2017-04-05 14:00:49 +0200

    Disable autoprefixer for now

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,12 @@
 var autoprefixer = require('autoprefixer');
 
 module.exports = {
-    plugins: [
-        autoprefixer({
-            browsers: ['last 3 versions', '> 3%']
-        })
-    ]
+    // Disable autoprefixer for now since react-selectize (used in bridgedbjs) has issues with display: box
+    // Follow https://github.com/furqanZafar/react-selectize/pull/130
+    // TODO: Re-enable when new release or react-selectize (>2.1.0)
+    // plugins: [
+    //     autoprefixer({
+    //         browsers: ['last 3 versions', '> 3%']
+    //     })
+    // ]
 };
