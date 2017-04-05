34196a19b578c5e32e236d11bde41e83902ac07c
Jacob Windsor <jacob.windsor@ymail.com>
2017-04-05 14:06:40 +0200

    Add autoprefixer back. Warnings are fine.

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,11 @@
 var autoprefixer = require('autoprefixer');
 
 module.exports = {
-    // Disable autoprefixer for now since react-selectize (used in bridgedbjs) has issues with display: box
+    // Autoprefixer e (used in bridgedbjs) has issues with display: box giving warning on build
     // Follow https://github.com/furqanZafar/react-selectize/pull/130
-    // TODO: Re-enable when new release or react-selectize (>2.1.0)
-    // plugins: [
-    //     autoprefixer({
-    //         browsers: ['last 3 versions', '> 3%']
-    //     })
-    // ]
+    plugins: [
+        autoprefixer({
+            browsers: ['last 3 versions', '> 3%']
+        })
+    ]
 };
