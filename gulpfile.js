a8e60ca04216f38835e6a71935c64e028b92d3ba
Jacob Windsor <jacob.windsor@ymail.com>
2017-04-05 13:41:40 +0200

    Extract CSS into external file in dist rather using gulp to concat it

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,6 @@ var copy = require('gulp-copy');
 var concatCss = require('gulp-concat-css');
 var cleanCSS = require('gulp-clean-css');
 
-// Create an external CSS style that can optionally be used by devs instead of the webpack style loader
-// This is useful when using Angular CLI since all styles must be specified in the styles property of a component
-// See: https://github.com/angular/angular-cli/issues/1459
-// Note: the typestyles will still be imported fine since they are not css files
-// TODO: Add that this can be used in the README with an example for Angular CLI
-gulp.task('create-styles', function(){
-    return gulp.src('./src/styles.css')
-        .pipe(concatCss('styles.css'))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('./lib'));
-});
-
 // Copy all assets that aren't js or ts
 gulp.task('copy:assets', function() {
     return gulp.src('./src/**/!(*.ts|*.tsx|*.js|*.jsx|*.map|*.log)')
@@ -24,4 +12,4 @@ gulp.task('copy:assets', function() {
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['copy:assets', 'create-styles']);
+gulp.task('default', ['copy:assets']);
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 const path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: './src/index.ts',
@@ -19,7 +20,18 @@ module.exports = {
     },
     module: {
         rules: [
-            { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader']},
+            // Create an external stylesheet rather than inlined to support Angular CLI users
+            // In Angular CLI, all styles must be specified in the styles property of a component
+            // See: https://github.com/angular/angular-cli/issues/1459
+            // Note: the typestyles will still be imported fine since they are not css files
+            // Angular CLI users should add 'dist/style.css' into their component styles
+            // TODO: Add an example of this in the README
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'postcss-loader']
+                })
+            },
             { test: /\.json$/, use: 'json-loader'},
             { test: /\.ts(x?)$/, use: 'ts-loader?' + JSON.stringify({
                 compilerOptions: {
@@ -45,6 +57,10 @@ module.exports = {
             output: {
                 comments: false
             }
+        }),
+        new ExtractTextPlugin({
+            filename: "style.css",
+            allChunks: true
         })
     ]
 };
