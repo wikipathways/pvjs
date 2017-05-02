8461f123a17a8020cba7afdb3424b7d6ba829a8b
Jacob Windsor <jacob.windsor@ymail.com>
2017-05-02 13:49:16 +0200

    Add wikipathways organisation and ignore dist and lib

--- lib/Kaavio/components/Text.d.ts
+++ lib/Kaavio/components/Text.d.ts
@@ -2,8 +2,6 @@
 import * as React from 'react';
 import { TextProps } from "../typings";
 export declare class Text extends React.Component<any, any> {
-    svgRef: any;
     constructor(props: TextProps);
-    componentDidMount(): void;
     render(): JSX.Element;
 }
