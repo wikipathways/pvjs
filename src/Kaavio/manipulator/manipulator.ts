3defe2ee45a2cb48b624f7b5749014005c9d4c19
Jacob Windsor <jacob.windsor@ymail.com>
2017-06-06 10:55:53 +0200

    Remove typings

--- src/Kaavio/manipulator/manipulator.ts
+++ src/Kaavio/manipulator/manipulator.ts
@@ -4,45 +4,14 @@ import {Zoomer} from "./zoomer";
 import {Panner} from "./panner";
 import * as ReactDOM from 'react-dom';
 
-export interface Manipulator {
-    toggleHighlight(entity_id: string, color: string): void;
-    highlightOn(entity_id: string, color: string): void;
-    highlightOff(entity_id:string): void;
-    resetHighlighted(exclude?: string[]): void;
-    hide(entity_id:string): void;
-    show(entity_id: string): void;
-    resetHidden(exclude?: string[]): void;
-    panTo(entity_id: string | string[], animate: boolean): void;
-    pan(coordinates: {x: number, y: number}, animate: boolean): void;
-    resetPan(): void;
-    zoom(zoom_perc: number, animate: boolean): void;
-    zoomOn(entity_id: string, animate: boolean);
-    zoomIn(): void;
-    zoomOut(): void;
-    resetZoom(): void;
-    reset(): void;
-    getEntities(): {
-        id: string;
-        kaavioType: string;
-        textContent: string;
-        types: string[];
-    }[]
-}
-
-/**
- * Factory for the manipulation API
- * @param kaavioRef
- * @param panZoomRef
- * @param diagramRef
-*/
-export function getManipulator(kaavioRef, panZoomRef, diagramRef): Manipulator {
+export function getManipulator(kaavioRef, panZoomRef, diagramRef) {
   const highlighter = new Highlighter(kaavioRef);
   const hider = new Hider(kaavioRef);
   const diagram = ReactDOM.findDOMNode(diagramRef);
   const zoomer = new Zoomer(kaavioRef, panZoomRef, diagram);
   const panner = new Panner(kaavioRef, panZoomRef, diagram);
 
-  return <Manipulator>{
+  return {
       toggleHighlight: highlighter.toggleHighlight.bind(highlighter),
       highlightOn: highlighter.highlightOn.bind(highlighter),
       highlightOff: highlighter.highlightOff.bind(highlighter),
