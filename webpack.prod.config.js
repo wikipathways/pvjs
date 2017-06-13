fa010600add15a64f6ef9abae172724ab617f562
Jacob Windsor <jacob.windsor@ymail.com>
2017-06-13 11:24:46 +0200

    improve webpack config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -52,11 +52,20 @@ module.exports = {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
         new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            output: {
-                comments: false
-            }
+            beautify: false,
+            mangle: {
+                screw_ie8: true,
+                keep_fnames: true
+            },
+            compress: {
+                screw_ie8: true
+            },
+            comments: false
         }),
         new ExtractTextPlugin({
             filename: "style.css",
