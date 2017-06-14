2528f5084f471f5667207fb6388d91e7a279a3b7
Jacob Windsor <jacob.windsor@ymail.com>
2017-06-14 10:49:59 +0200

    Fix highlighting in FF

--- src/Kaavio/filters/highlighter.tsx
+++ src/Kaavio/filters/highlighter.tsx
@@ -4,10 +4,10 @@ export function highlighter(id, color) {
         url: 'highlighter-for-' + id,
         filter: (
                 <filter id={"highlighter-for-" + id} key={"highlighter-for-" + id}>
-                    <feColorMatrix in="StrokePaint"
+                    <feColorMatrix in="SourceGraphic"
                                    type="saturate" values="0" result="toHighlight" /> {/* Desaturate all colours before highlighting */}
                     <feFlood floodColor={color} floodOpacity="0.5" result="highlight" />
-                    <feComposite in="highlight" in2="toHighlight" operator="atop" result="output" />
+                    <feComposite in="highlight" in2="toHighlight" operator="atop"/>
                 </filter>
             )
     }
