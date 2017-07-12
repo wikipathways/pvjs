68e9d51209a4eda23b9e1f0069b01acc71080a07
Jacob Windsor <jacob.windsor@ymail.com>
2017-07-12 10:04:32 +0100

    Remove compile files

--- src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import 'document-register-element';
-import 'whatwg-fetch';
-import 'roboto-fontface/css/roboto/roboto-fontface.css';
-import './wrappers/pvjs.webcomponent';
-export { loadDiagram } from './wrappers/vanilla';
-export { Pvjs } from './Pvjs';
