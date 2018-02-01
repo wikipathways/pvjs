ccccb181d2aae5cefd16248033b3aba00db8c421
Anders Riutta <git@andersriutta.com>
2018-01-31 22:31:01 -0800

    Make CSS handling consistent with browser.

--- src/cli.tsx
+++ src/cli.tsx
@@ -1,8 +1,26 @@
 import "source-map-support/register";
 
 import { createJson2SvgCLI } from "kaavio/lib/createJson2SvgCLI";
-import * as customStyleHTML from "./customStyleHTML.css";
-import * as customStyleSVG from "./customStyleSVG.css";
+
+// TODO: for both css imports below, I just want the CSS string,
+// but the .css -> .d.ts compiler doesn't specify a default export.
+// Importing as * as blah yields an object, not a string.
+// Both require and import + extract default work, but they look ugly.
+
+// TODO: does it make sense to import the CSS for the HTML?
+// Doesn't the CLI only make SVG?
+/*
+//import * as customStyleHTML from "./customStyleHTML.css";
+import * as customStyleHTMLModule from "./customStyleHTML.css";
+const customStyleHTML = customStyleHTMLModule["default"];
+//*/
+const customStyleHTML = require("./customStyleHTML.css");
+/*
+//import customStyleSVG from "./customStyleSVG.css";
+import * as customStyleSVGModule from "./customStyleSVG.css";
+const customStyleSVG = customStyleSVGModule["default"];
+//*/
+const customStyleSVG = require("./customStyleSVG.css");
 
 import * as edgeDrawerMap from "./drawers/edges/index";
 import * as filterDrawerMap from "./drawers/filters/index";
