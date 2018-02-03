4d147432995206bb59af9ec6cf6b7c8bb631a6cd
Anders Riutta <git@andersriutta.com>
2018-02-03 15:19:02 -0800

    Improved CLI to allow for using themes.

--- src/cli.tsx
+++ src/cli.tsx
@@ -2,19 +2,10 @@ import "source-map-support/register";
 
 import { createJson2SvgCLI } from "kaavio/lib/createJson2SvgCLI";
 
-// TODO: for both css imports below, I just want the CSS string,
+// TODO: for the css below, I just want the CSS string,
 // but the .css -> .d.ts compiler doesn't specify a default export.
 // Importing as * as blah yields an object, not a string.
 // Both require and import + extract default work, but they look ugly.
-
-// TODO: does it make sense to import the CSS for the HTML?
-// Doesn't the CLI only make SVG?
-/*
-//import * as customStyleHTML from "./customStyleHTML.css";
-import * as customStyleHTMLModule from "./customStyleHTML.css";
-const customStyleHTML = customStyleHTMLModule["default"];
-//*/
-const customStyleHTML = require("./html.css");
 /*
 //import customStyleSVG from "./customStyleSVG.css";
 import * as customStyleSVGModule from "./customStyleSVG.css";
@@ -29,7 +20,8 @@ import { Icons as IconsPlain } from "./drawers/IconBundle.plain";
 import { Icons as IconsPretty } from "./drawers/IconBundle.pretty";
 import * as markerDrawerMap from "./drawers/markers/index";
 
-const plain = false;
+/*
+const plain = true;
 if (plain) {
   createJson2SvgCLI("pvjs", {
     customStyleHTML,
@@ -49,3 +41,23 @@ if (plain) {
     markerDrawerMap
   });
 }
+//*/
+
+createJson2SvgCLI("pvjs", [
+  {
+    name: "plain",
+    style: customStyleSVGPlain,
+    edgeDrawerMap,
+    filterDrawerMap,
+    Icons: IconsPlain,
+    markerDrawerMap
+  },
+  {
+    name: "pretty",
+    style: customStyleSVGPretty,
+    edgeDrawerMap,
+    filterDrawerMap,
+    Icons: IconsPretty,
+    markerDrawerMap
+  }
+]);
